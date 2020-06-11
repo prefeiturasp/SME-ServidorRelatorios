8fa2b51d0a688199ec748892c7c4146f1699913a
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-06-11 12:10:51 -0300

    Ajustes nos encondings

--- jasper_server/app-entrypoint.sh
+++ jasper_server/app-entrypoint.sh
@@ -7,10 +7,9 @@ print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize tomcat mysql-client jasperreports
-    info "Starting gosu... "
+    info "Starting Jasper Server Edited... "
 fi
 
 cp web.xml bitnami/tomcat/data/jasperserver/WEB-INF/
 
 exec tini -- "$@"
-
