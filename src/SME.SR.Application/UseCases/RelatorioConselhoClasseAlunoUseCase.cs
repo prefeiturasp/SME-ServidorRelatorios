2b7c919890b6847d45dec5fb5b019c56b390356a
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-06-14 23:42:07 -0300

    ajustes

--- src/SME.SR.Application/UseCases/RelatorioConselhoClasseAlunoUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioConselhoClasseAlunoUseCase.cs
@@ -36,7 +36,7 @@ namespace SME.SR.Application
                     SentrySdk.CaptureMessage("4.09 Obtendo relatorio.. - RelatorioConselhoClasseAlunoUseCase");
                     //SentrySdk.AddBreadcrumb("Obtendo relatório..", "5 - RelatorioConselhoClasseAlunoUseCase");
 
-                    RelatorioConselhoClasseArray relatorio = await mediator.Send(relatorioQuery);
+                    //RelatorioConselhoClasseArray relatorio = await mediator.Send(relatorioQuery);
 
                     SentrySdk.CaptureMessage("5.1 Obtive relatorio.. - RelatorioConselhoClasseAlunoUseCase");
                     //SentrySdk.AddBreadcrumb("Obtive o relatório", "5 - RelatorioConselhoClasseAlunoUseCase");
