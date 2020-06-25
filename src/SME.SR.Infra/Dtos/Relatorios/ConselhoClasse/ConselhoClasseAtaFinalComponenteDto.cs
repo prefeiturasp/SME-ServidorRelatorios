7e45d7dca2603438b81f43d460dfb72407fa639d
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-25 16:42:29 -0300

    17403 - Query Componentes da Turma - Ata Final

---
+++ src/SME.SR.Infra/Dtos/Relatorios/ConselhoClasse/ConselhoClasseAtaFinalComponenteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SR.Infra
+{
+    public class ConselhoClasseAtaFinalComponenteDto
+    {
+        public long Codigo { get; set; }
+        public string Nome { get; set; }
+        public string GrupoMatriz { get; set; }
+    }
+}
