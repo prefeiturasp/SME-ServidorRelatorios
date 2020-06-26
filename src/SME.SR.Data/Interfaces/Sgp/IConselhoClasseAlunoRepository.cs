2d79cf6399f066d29701a8a3ad890d9bd2463d19
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-26 15:12:03 -0300

    17407 - Query Parecer Conclusivo da Turma - Ata Final

--- src/SME.SR.Data/Interfaces/Sgp/IConselhoClasseAlunoRepository.cs
+++ src/SME.SR.Data/Interfaces/Sgp/IConselhoClasseAlunoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using SME.SR.Infra;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SR.Data.Interfaces
@@ -6,6 +7,8 @@ namespace SME.SR.Data.Interfaces
     public interface IConselhoClasseAlunoRepository
     {
         Task<string> ObterParecerConclusivo(long conselhoClasseId, string codigoAluno);
+
+        Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterParecerConclusivoPorTurma(string turmaCodigo);
 
         Task<RecomendacaoConselhoClasseAluno> ObterRecomendacoesPorFechamento(long fechamentoTurmaId, string codigoAluno);
     }
