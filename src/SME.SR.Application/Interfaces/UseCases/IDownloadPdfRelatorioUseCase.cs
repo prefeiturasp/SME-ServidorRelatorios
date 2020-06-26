d7b4e13d45bd47fa1d744d09d285aa7a8fbb34a7
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-26 15:42:25 -0300

    Adicionando endpoint de download do pdf

---
+++ src/SME.SR.Application/Interfaces/UseCases/IDownloadPdfRelatorioUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public interface IDownloadPdfRelatorioUseCase
+    {
+        Task<byte[]> Executar(Guid codigoCorrelacao);
+    }
+}
---
+++ src/SME.SR.Infra/Excecoes/NegocioException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SME.SR.Infra
+{
+    [Serializable]
+    public class NegocioException : Exception
+    {
+        public NegocioException(string message) : base(message)
+        {
+        }
+
+        protected NegocioException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
