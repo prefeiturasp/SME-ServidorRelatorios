7d92d5a86e2ff5aad938d075f7a978643eedb95f
Everton Nogueira <nogueiraever>
2020-06-30 22:55:15 -0300

    Ajuste para notificar usuário em caso de erro na geração do relatório

--- src/SME.SR.Infra/Dtos/Relatorios/FiltroRelatorioDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/FiltroRelatorioDto.cs
@@ -8,7 +8,7 @@ namespace SME.SR.Infra
         public string Action { get; set; }
         public object Filtros { get; set; }
         public Guid CodigoCorrelacao { get; set; }
-
+        public string UsuarioLogadoRF { get; set; }
         public T ObterObjetoFiltro<T>() where T: class
         {
             return JsonConvert.DeserializeObject<T>(Filtros.ToString());
