7cc91c550556fc175be0e45f59c5990e98b202b0
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-03 15:29:09 -0300

    Implementação parcial do Handler do Cabeçalho do Historico Escolar

---
+++ src/SME.SR.Application/Queries/HistoricoEscolar/ObterCabecalhoHistoricoEscolar/ObterCabecalhoHistoricoEscolarQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SR.Data;
+using SME.SR.Infra;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class ObterCabecalhoHistoricoEscolarQueryHandler : IRequestHandler<ObterCabecalhoHistoricoEscolarQuery, CabecalhoDto>
+    {
+        private IObterCabecalhoHistoricoEscolarRepository obterCabecalhoHistoricoEscolarRepository;
+
+        public ObterCabecalhoHistoricoEscolarQueryHandler(IObterCabecalhoHistoricoEscolarRepository _obterCabecalhoHistoricoEscolarRepository)
+        {
+            obterCabecalhoHistoricoEscolarRepository = _obterCabecalhoHistoricoEscolarRepository;
+        }
+
+        public async Task<CabecalhoDto> Handle(ObterCabecalhoHistoricoEscolarQuery request, CancellationToken cancellationToken)
+        {
+            return await obterCabecalhoHistoricoEscolarRepository.ObterCabecalhoHistoricoEscolar(request.codigoDre);
+        }
+    }
+}
