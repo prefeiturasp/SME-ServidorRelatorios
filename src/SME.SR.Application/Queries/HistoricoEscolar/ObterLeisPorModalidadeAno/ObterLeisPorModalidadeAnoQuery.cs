f9884214d18d526a2940e89fa6a46311443d992a
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-03 23:59:13 -0300

    Implementação parcial de QueryHandler para Obter Leis  por ano e modalidade

---
+++ src/SME.SR.Application/Queries/HistoricoEscolar/ObterLeisPorModalidadeAno/ObterLeisPorModalidadeAnoQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SME.SR.Infra;
+
+namespace SME.SR.Application
+{
+    public class ObterLeisPorModalidadeAnoQuery : IRequest<string>
+    {
+        public int AnoLetivo { get; set; }
+
+        public Modalidade? Modalidade { get; set; }
+
+    }
+}
