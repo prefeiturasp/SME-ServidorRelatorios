de7dd0d03b3b6abf5da9240ba653008eac1789a9
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-07-07 21:47:16 -0300

    inicio do query handler

---
+++ src/SME.SR.Application/Queries/HistoricoEscolar/ObterNotasEFrequenciasDosAlunos/ObterNotasEFrequenciasDosAlunosQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+
+namespace SME.SR.Application
+{
+    public class ObterNotasEFrequenciasDosAlunosQuery : IRequest<IEnumerable<HistoricoEscolarDTO>>
+    {
+        public string CodigoTurma { get; set; }
+        public string[] CodigoAlunos { get; set; }
+    }
+}
---
+++ src/SME.SR.Application/Queries/HistoricoEscolar/ObterNotasEFrequenciasDosAlunos/ObterNotasEFrequenciasDosAlunosQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class ObterNotasEFrequenciasDosAlunosQueryHandler : IRequestHandler<ObterNotasEFrequenciasDosAlunosQuery, IEnumerable<HistoricoEscolarDTO>>
+    {
+        public ObterNotasEFrequenciasDosAlunosQueryHandler()
+        {
+        }
+
+        public Task<IEnumerable<HistoricoEscolarDTO>> Handle(ObterNotasEFrequenciasDosAlunosQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
