d21b090e6f0a585397080eaddee3e66e1f8ce25c
Rafael Losi <rafael.losi@amcom.com.br>
2020-07-11 17:54:36 -0300

    19236 - Query Faltas/Frequencia Anual

---
+++ src/SME.SR.Application/Queries/Frequencia/ObterFrequenciaAlunoGlobalPorComponetnesBimestresETurmas/ObterFrequenciaAlunoGlobalPorComponetnesBimestresETurmasQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SR.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Application
+{
+    public class ObterFrequenciaAlunoGlobalPorComponetnesBimestresETurmasQuery: IRequest<IEnumerable<FrequenciaAluno>>
+    {
+        public ObterFrequenciaAlunoGlobalPorComponetnesBimestresETurmasQuery(IEnumerable<Turma> turmas, IEnumerable<long> componentesCurriculares)
+        {
+            Turmas = turmas;
+            ComponentesCurriculares = componentesCurriculares;
+        }
+
+        public IEnumerable<Turma> Turmas { get; set; }
+        public IEnumerable<long> ComponentesCurriculares { get; set; }
+    }
+}
