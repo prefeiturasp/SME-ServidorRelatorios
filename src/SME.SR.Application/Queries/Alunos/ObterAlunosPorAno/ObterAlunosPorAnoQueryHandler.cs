23640771458d36540b0e9b13d575b3d6407ec3b4
Rafael Losi <rafael.losi@amcom.com.br>
2020-07-11 12:59:45 -0300

    19235 - Query de frequencia por lista de turma, componente e bimestres

--- src/SME.SR.Application/Queries/Alunos/ObterAlunosPorAno/ObterAlunosPorAnoQueryHandler.cs
+++ src/SME.SR.Application/Queries/Alunos/ObterAlunosPorAno/ObterAlunosPorAnoQueryHandler.cs
@@ -12,25 +12,22 @@ namespace SME.SR.Application
 {
     public class ObterAlunosPorAnoQueryHandler : IRequestHandler<ObterAlunosPorAnoQuery, IEnumerable<AlunoTurma>>
     {
-        private readonly IMediator mediator;
         private readonly IAlunoRepository alunoRepository;
 
-        public ObterAlunosPorAnoQueryHandler(IMediator mediator, IAlunoRepository alunoRepository)
+        public ObterAlunosPorAnoQueryHandler(IAlunoRepository alunoRepository)
         {
-            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             this.alunoRepository = alunoRepository ?? throw new ArgumentNullException(nameof(alunoRepository));
         }
 
         public async Task<IEnumerable<AlunoTurma>> Handle(ObterAlunosPorAnoQuery request, CancellationToken cancellationToken)
         {
-            var turmas = await mediator.Send(new ObterTurmasPorAnoQuery(request.AnoLetivo, request.AnosEscolares));
-            var alunos = await alunoRepository.ObterPorCodigosTurma(turmas.Select(a => a.Codigo).ToArray());
+            var alunos = await alunoRepository.ObterPorCodigosTurma(request.TurmasCodigos.ToArray());
 
             return alunos.Select(a => new AlunoTurma()
             {
                 Nome = a.NomeAluno,
                 NumeroChamada = a.NumeroAlunoChamada,
-                Turma = turmas.First(t => t.Codigo == a.CodigoTurma.ToString()).Nome
+                TurmaCodigo = a.CodigoTurma.ToString()
             });
         }
     }
---
+++ src/SME.SR.Application/Queries/RelatorioFaltasFrequencia/ObterRelatorioFaltasFrequencias/ObterRelatorioFaltasFrequenciasQueryHandler.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using SME.SR.Data;
+using SME.SR.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class ObterRelatorioFaltasFrequenciasQueryHandler : IRequestHandler<ObterRelatorioFaltasFrequenciasQuery, RelatorioFaltasFrequenciasDto>
+    {
+        private readonly IMediator mediator;
+
+        public ObterRelatorioFaltasFrequenciasQueryHandler(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<RelatorioFaltasFrequenciasDto> Handle(ObterRelatorioFaltasFrequenciasQuery request, CancellationToken cancellationToken)
+        {
+            var turmas = await mediator.Send(new ObterTurmasPorAnoQuery(request.AnoLetivo, request.AnosEscolares));
+            var alunos = await ObterAlunosPorAno(turmas);
+            var faltasFrequencias = await ObterFaltasEFrequencias(turmas.Select(a => a.Codigo), request.Bimestres, request.ComponentesCurriculares);
+
+            return await Task.FromResult(new RelatorioFaltasFrequenciasDto());
+        }
+
+        private async Task<IEnumerable<FrequenciaAluno>> ObterFaltasEFrequencias(IEnumerable<string> turmasCodigos, IEnumerable<int> bimestresFiltro, IEnumerable<long> componentesCurriculares)
+        {
+            var faltasFrequenciasAlunos = new List<FrequenciaAluno>();
+
+            // Obter bimestres diferentes de "Final"
+            var bimestres = bimestresFiltro.Where(c => c != 0);
+            if (bimestres != null && bimestres.Any())
+            {
+
+                faltasFrequenciasAlunos.AddRange(await mediator.Send(new ObterFrequenciaAlunoPorComponentesBimestresETurmasQuery(turmasCodigos, bimestres, componentesCurriculares)));
+            }
+
+            // Verifica se foi solicitado bimestre final
+            if (bimestresFiltro.Any(c => c == 0))
+            {
+                // TODO task 19236 query de faltas/frequencia anual
+            }
+
+            return faltasFrequenciasAlunos;
+        }
+
+        private async Task<IEnumerable<RelatorioFaltaFrequenciaAlunoDto>> ObterAlunosPorAno(IEnumerable<Turma> turmas)
+        {
+            var alunos = await mediator.Send(new ObterAlunosPorAnoQuery(turmas.Select(a => a.Codigo)));
+            return alunos.Select(a => new RelatorioFaltaFrequenciaAlunoDto()
+            {
+                Nome = a.Nome,
+                NomeTurma = turmas.First(t => t.Codigo == t.Codigo).Nome,
+                Numero = a.NumeroChamada
+            });
+        }
+    }
+}
