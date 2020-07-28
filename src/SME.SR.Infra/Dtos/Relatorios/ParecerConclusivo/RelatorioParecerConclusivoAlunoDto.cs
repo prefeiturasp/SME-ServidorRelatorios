c871ba7f9eff29604bed3a0806f088803c718551
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-28 16:16:04 -0300

    Ajustes nos nomes das prorpiedades

--- src/SME.SR.Infra/Dtos/Relatorios/ParecerConclusivo/RelatorioParecerConclusivoAlunoDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/ParecerConclusivo/RelatorioParecerConclusivoAlunoDto.cs
@@ -7,8 +7,8 @@
 
         }
         public string TurmaNome { get; set; }
-        public string NumeroChamadaAluno { get; set; }
-        public string NomeCompletoAluno { get; set; }
-        public string DescricaoParecerConclusico { get; set; }
+        public string AlunoNumeroChamada { get; set; }
+        public string AlunoNomeCompleto { get; set; }
+        public string ParecerConclusivoDescricao { get; set; }
     }
 }
