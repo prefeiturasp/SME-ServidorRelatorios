5977b9f5804af3ea7b734e58f88c873bd24fe3ba
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-28 16:07:47 -0300

    Implementação de DTO para o relatório de parecer conclusivo

---
+++ src/SME.SR.Infra/Dtos/Relatorios/ParecerConclusivo/RelatorioParecerConclusivoUeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioParecerConclusivoUeDto
+    {
+        public RelatorioParecerConclusivoUeDto()
+        {
+            Ciclos = new List<RelatorioParecerConclusivoCicloDto>();
+        }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public List<RelatorioParecerConclusivoCicloDto> Ciclos { get; set; }
+    }
+}
