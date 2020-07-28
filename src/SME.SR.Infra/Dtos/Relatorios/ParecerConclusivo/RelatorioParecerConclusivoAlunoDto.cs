d8fba395e51d28f1d7a3f27c3174d0a9396210b0
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-28 15:22:59 -0300

    Implementação de DTO para o relatório de Parecer Conclusivo

---
+++ src/SME.SR.Infra/Dtos/Relatorios/ParecerConclusivo/RelatorioParecerConclusivoAlunoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioParecerConclusivoAlunoDto
+    {
+        public RelatorioParecerConclusivoAlunoDto()
+        {
+
+        }
+        public string TurmaNome { get; set; }
+        public string NumeroChamadaAluno { get; set; }
+        public string NomeCompletoAluno { get; set; }
+        public string DescricaoParecerConclusico { get; set; }
+    }
+}
