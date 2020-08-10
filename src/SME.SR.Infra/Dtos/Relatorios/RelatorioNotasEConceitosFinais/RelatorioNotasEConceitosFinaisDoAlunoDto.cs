a5aa436938a0ea2fe898aad4c0e2b09fcf6d8607
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-08-10 15:45:33 -0300

    Implementação preliminar do dto

---
+++ src/SME.SR.Infra/Dtos/Relatorios/RelatorioNotasEConceitosFinais/RelatorioNotasEConceitosFinaisDoAlunoDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioNotasEConceitosFinaisDoAlunoDto
+    {
+        public RelatorioNotasEConceitosFinaisDoAlunoDto(string turmaNome, string numeroChamadaAluno, string nomeCompletoAluno, string notaConceito)
+        {
+            TurmaNome = turmaNome;
+            NumeroChamadaAluno = numeroChamadaAluno;
+            NomeCompletoAluno = nomeCompletoAluno;
+            NotaConceito = notaConceito;
+        }
+
+        public string TurmaNome { get; set; }
+        public string NumeroChamadaAluno { get; set; }
+        public string NomeCompletoAluno { get; set; }
+        public string NotaConceito { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/RelatorioNotasEConceitosFinais/RelatorioNotasEConceitosFinaisDreDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioNotasEConceitosFinaisDreDto
+    {
+        public RelatorioNotasEConceitosFinaisDreDto()
+        {
+            Ues = new List<RelatorioNotasEConceitosFinaisUeDto>();
+        }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public List<RelatorioNotasEConceitosFinaisUeDto> Ues { get; set; }
+    }
+}
