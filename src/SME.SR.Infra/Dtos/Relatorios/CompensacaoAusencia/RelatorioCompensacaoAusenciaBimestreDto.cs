4c540dd2af68c4b59accb8eb125d0a0525a122ea
eliton-amcom <eliton.santos@amcom.com.br>
2020-08-22 14:00:00 -0300

    22433 - Dto relatório compensação de ausência

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CompensacaoAusencia/RelatorioCompensacaoAusenciaBimestreDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioCompensacaoAusenciaBimestreDto
+    {
+        public RelatorioCompensacaoAusenciaBimestreDto()
+        {
+            Componentes = new List<RelatorioCompensacaoAusenciaComponenteDto>();
+        }
+        public string Nome { get; set; }
+
+        public List<RelatorioCompensacaoAusenciaComponenteDto> Componentes { get; set; }
+
+    }
+}
