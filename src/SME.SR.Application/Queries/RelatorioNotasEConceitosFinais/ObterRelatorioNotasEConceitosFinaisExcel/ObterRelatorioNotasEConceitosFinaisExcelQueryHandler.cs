9408ef1952d9fdb3ffa58f36d3052f0fdaf1d5dc
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-09-04 17:00:56 -0300

    Emissão relatório Excel Notas e Conceitos Finais

---
+++ src/SME.SR.Application/Queries/RelatorioNotasEConceitosFinais/ObterRelatorioNotasEConceitosFinaisExcel/ObterRelatorioNotasEConceitosFinaisExcelQueryHandler.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class ObterRelatorioNotasEConceitosFinaisExcelQueryHandler : IRequestHandler<ObterRelatorioNotasEConceitosFinaisExcelQuery, IEnumerable<RelatorioNotasEConceitosFinaisExcelDto>>
+    {
+        public async Task<IEnumerable<RelatorioNotasEConceitosFinaisExcelDto>> Handle(ObterRelatorioNotasEConceitosFinaisExcelQuery request, CancellationToken cancellationToken)
+        {
+            var listaNotasEConceitosFinais = new List<RelatorioNotasEConceitosFinaisExcelDto>();
+
+            foreach (var dre in request.RelatorioNotasEConceitosFinais.Dres)
+            {
+                foreach (var ue in dre.Ues)
+                {
+                    foreach (var ano in ue.Anos)
+                    {
+                        foreach (var bimestre in ano.Bimestres)
+                        {
+                            foreach (var componente in bimestre.ComponentesCurriculares)
+                            {
+                                foreach (var aluno in componente.NotaConceitoAlunos)
+                                {
+                                    listaNotasEConceitosFinais.Add(ObterRelatorioNotasEConceito(dre.Nome, ue.Nome, bimestre.Nome,
+                                                                                            ano.Nome, aluno.TurmaNome, componente.Nome,
+                                                                                            aluno.AlunoCodigo, aluno.AlunoNomeCompleto, aluno.NotaConceito));
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            return await Task.FromResult(listaNotasEConceitosFinais);
+        }
+
+      
+        private RelatorioNotasEConceitosFinaisExcelDto ObterRelatorioNotasEConceito(string dreNome, string ueNome, string bimestre,
+                                                                                      string ano, string turma, string componenteCurricular,
+                                                                                      int alunoCodigo, string alunoNome, string notaConceito)
+        {
+            RelatorioNotasEConceitosFinaisExcelDto relatorioDto = new RelatorioNotasEConceitosFinaisExcelDto();
+
+            relatorioDto.DreNome = dreNome;
+            relatorioDto.UnidadeEscolarNome = ueNome;
+            relatorioDto.Bimestre = bimestre;
+            relatorioDto.Ano = ano;
+            relatorioDto.Turma = turma;
+            relatorioDto.ComponenteCurricular = componenteCurricular;
+            relatorioDto.EstudanteCodigo = alunoCodigo.ToString();
+            relatorioDto.EstudanteNome = alunoNome;
+            relatorioDto.NotaConceito = notaConceito;
+
+            return relatorioDto;
+        }
+    }
+}
---
+++ src/SME.SR.Application/Queries/RelatorioNotasEConceitosFinais/ObterRelatorioNotasEConceitosFinaisPdf/ObterRelatorioNotasEConceitosFinaisPdfQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SR.Infra;
+
+namespace SME.SR.Application
+{
+    public class ObterRelatorioNotasEConceitosFinaisPdfQuery : IRequest<RelatorioNotasEConceitosFinaisDto>
+    {
+        public FiltroRelatorioNotasEConceitosFinaisDto FiltroRelatorioNotasEConceitosFinais { get; set; }
+
+        public ObterRelatorioNotasEConceitosFinaisPdfQuery(FiltroRelatorioNotasEConceitosFinaisDto filtroRelatorioNotasEConceitosFinais)
+        {
+            this.FiltroRelatorioNotasEConceitosFinais = FiltroRelatorioNotasEConceitosFinais;
+        }
+    }
+}
