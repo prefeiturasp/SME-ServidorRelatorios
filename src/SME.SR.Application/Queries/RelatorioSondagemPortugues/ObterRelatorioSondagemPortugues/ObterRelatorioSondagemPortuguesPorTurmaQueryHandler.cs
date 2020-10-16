d57ff323c06cb4073a39c4eb158cd9a701d18a99
Rezende <rezehnde@gmail.com>
2020-10-16 10:15:29 -0300

    Remoção do uso de repositório para buscar Usuário

--- src/SME.SR.Application/Queries/RelatorioSondagemPortugues/ObterRelatorioSondagemPortugues/ObterRelatorioSondagemPortuguesPorTurmaQueryHandler.cs
+++ src/SME.SR.Application/Queries/RelatorioSondagemPortugues/ObterRelatorioSondagemPortugues/ObterRelatorioSondagemPortuguesPorTurmaQueryHandler.cs
@@ -15,16 +15,13 @@ namespace SME.SR.Application
     public class ObterRelatorioSondagemPortuguesPorTurmaQueryHandler : IRequestHandler<ObterRelatorioSondagemPortuguesPorTurmaQuery, RelatorioSondagemPortuguesPorTurmaRelatorioDto>
     {
         private readonly IRelatorioSondagemPortuguesPorTurmaRepository relatorioSondagemPortuguesPorTurmaRepository;
-        private readonly IUsuarioRepository usuarioRepository;
         private readonly IMediator mediator;
 
         public ObterRelatorioSondagemPortuguesPorTurmaQueryHandler(
             IRelatorioSondagemPortuguesPorTurmaRepository relatorioSondagemPortuguesPorTurmaRepository,
-            IUsuarioRepository usuarioRepository,
             IMediator mediator)
         {
             this.relatorioSondagemPortuguesPorTurmaRepository = relatorioSondagemPortuguesPorTurmaRepository ?? throw new ArgumentNullException(nameof(relatorioSondagemPortuguesPorTurmaRepository));
-            this.usuarioRepository = usuarioRepository ?? throw new ArgumentNullException(nameof(usuarioRepository));
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
