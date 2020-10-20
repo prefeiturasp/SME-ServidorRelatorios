1f4952cb2c6e12c6ecf5c3ddd65ec744de3369e2
William Manoel dos Santos <william.santos@amcom.com.br>
2020-10-20 12:19:19 -0300

    criação de ropositorio e entidade

---
+++ src/SME.SR.Data/Models/AulaPrevista.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.SR.Data
+{
+    public class AulaPrevista
+    {
+        public long TipoCalendarioId { get; set; }
+
+        public string DisciplinaId { get; set; }
+
+        public string TurmaId { get; set; }
+    }
+}
