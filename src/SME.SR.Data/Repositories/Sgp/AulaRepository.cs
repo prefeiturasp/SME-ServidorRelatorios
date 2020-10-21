dd17e79d9aad32b79a370010dc6770f8d97f52af
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-10-21 18:31:21 -0300

    18611 - Repositorio

--- src/SME.SR.Data/Repositories/Sgp/AulaRepository.cs
+++ src/SME.SR.Data/Repositories/Sgp/AulaRepository.cs
@@ -42,15 +42,51 @@ namespace SME.SR.Data
             }
         }
 
-        
-
-        public async Task<bool> VerificaExisteAulaCadastrada(string turmaCodigo, string componenteCurricularId)
+        public async Task<AulaPrevista> ObterAulaPrevistaFiltro(long tipoCalendarioId, string turmaId, string disciplinaId)
         {
-            var query = @"select distinct 1 from aula where turma_id = @turmaCodigo and disciplina_id = @componenteCurricularId;";
+            var query = @"select * from aula_prevista ap
+                         where ap.tipo_calendario_id = @tipoCalendarioId and ap.turma_id = @turmaId and
+                               ap.disciplina_id = @disciplinaId;";            
 
             using (var conexao = new NpgsqlConnection(variaveisAmbiente.ConnectionStringSgp))
             {
-                return await conexao.QueryFirstOrDefaultAsync<bool>(query, new { turmaCodigo, componenteCurricularId });
+                return await conexao.QueryFirstOrDefaultAsync<AulaPrevista>(query, new { tipoCalendarioId, turmaId, disciplinaId });
+            }
+        }
+
+        public async Task<bool> VerificaExisteAulaCadastrada(long turmaId, string componenteCurricularId)
+        {
+            var query = @"select distinct 1 from aula inner join turma on aula.turma_id = turma.turma_id where turma.id = @turmaId and disciplina_id = @componenteCurricularId;";
+
+            using (var conexao = new NpgsqlConnection(variaveisAmbiente.ConnectionStringSgp))
+            {
+                return await conexao.QueryFirstOrDefaultAsync<bool>(query, new { turmaId, componenteCurricularId });
+            }
+        }
+
+        public Task<int> ObterQuantidadeAulas(long turmaId, string componenteCurricularId, string CodigoRF)
+        {
+            var query = @"select sum(a.quantidade) 
+                          from aula a 
+                         inner join periodo_escolar p on p.tipo_calendario_id = a.tipo_calendario_id
+ 						                    and a.data_aula between p.periodo_inicio and p.periodo_fim
+                         inner join registro_frequencia rf on rf.aula_id = a.id
+                          where a.tipo_calendario_id = @tipoCalendarioId
+                            and a.turma_id = @codigoTurma 
+                            and a.disciplina_id = @disciplinaId 
+                            and p.bimestre = @bimestre ";
+
+            var parametros = new
+            {
+                CodigoTurma = codigoTurma,
+                DisciplinaId = componenteCurricularCodigo,
+                TipoCalendarioId = tipoCalendarioId,
+                Bimestre = bimestre
+            };
+
+            using (var conexao = new NpgsqlConnection(variaveisAmbiente.ConnectionStringSgp))
+            {
+                return await conexao.QueryFirstOrDefaultAsync<int?>(query, parametros) ?? 0;
             }
         }
     }
