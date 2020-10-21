a8e8999b116b3a5f768451d62385d35ea8c333e3
William Manoel dos Santos <william.santos@amcom.com.br>
2020-10-21 09:15:31 -0300

    Ajuste no repositorio de aula prevista e registro da dependencia

---
+++ src/SME.SR.Data/Models/AulaPrevistaBimestre.cs
@@ -0,0 +1,13 @@
+﻿namespace SME.SR.Data
+{
+    public class AulaPrevistaBimestre
+    {
+        public long AulaPrevistaId { get; set; }
+
+        public AulaPrevista AulaPrevista { get; set; }
+
+        public int Previstas { get; set; }
+
+        public int Bimestre { get; set; }
+    }
+}
