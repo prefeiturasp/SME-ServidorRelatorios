bde29810a46fda7a35c3076f075a70e1eac07f54
wagnersantos-amcom <wagner.santos@amcom.com.br>
2020-10-23 11:28:17 -0300

    Criação do DTO do relatorio de capacidade de leitura por turma

---
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioSondagemPortuguesCapacidadeLeituraPorTurma/RelatorioSondagemPortuguesCapacidadeLeituraPorTurmaOrdemRespostasDto.cs
@@ -0,0 +1,22 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioSondagemPortuguesCapacidadeLeituraPorTurmaOrdemRespostasDto
+    {
+        private string _resposta { get; set; }
+        public int OrdemId { get; set; }
+        public int PerguntaId { get; set; }
+        public string Resposta { get { return TransformarSiglaResposta(_resposta); } set { _resposta = value; } }
+        private string TransformarSiglaResposta(string sigla)
+        {
+            return (sigla?.ToUpper()) switch
+            {
+                "S" => "Escreve convencionalmente",
+                "N" => "Não escreve convencionalmente",
+                "A" => "Acertou",
+                "E" => "Errou",
+                "NR" => "Não resolveu",
+                _ => sigla,
+            };
+        }
+    }
+}
