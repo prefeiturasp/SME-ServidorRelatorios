48e0f4026079faaed8f90893f96f790f64daecc1
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-10-26 19:06:46 -0300

    26561 - Criado Layout inicial do analítico no Mock

---
+++ src/SME.SR.Infra/Dtos/ControleGradeSintetico/DetalhamentoDivergencias/AulasTitularCJControleGradeSinteticoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class AulasTitularCJControleGradeSinteticoDto
+    {
+        public string ProfessorTitular { get; set; }
+        public int QuantidadeAulas { get; set; }
+        public string ProfessorCJ { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/ControleGradeSintetico/DetalhamentoDivergencias/AulasTitularCJDataControleGradeSinteticoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class AulasTitularCJDataControleGradeSinteticoDto
+    {
+        public string Data { get; set; }
+        public IEnumerable<AulasTitularCJControleGradeSinteticoDto> Divergencias { get; set; }
+    }
+}
