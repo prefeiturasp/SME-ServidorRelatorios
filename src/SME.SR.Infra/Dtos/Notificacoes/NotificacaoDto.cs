f6ee8ea5f37fec71fe6f5e1af576efe65bf1486a
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-12 13:43:32 -0300

    27499 - Criado layout inicial e DTO

---
+++ src/SME.SR.Infra/Dtos/Notificacoes/NotificacaoDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class NotificacaoDto
+    {
+        public NotificacaoDto()
+        {
+        }
+
+        public long Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Categoria { get; set; }
+        public string Tipo { get; set; }
+        public string Situacao { get; set; }
+        public DateTime DataRecebimento { get; set; }
+        public DateTime DataLeitura { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Notificacoes/RelatorioNotificacaoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioNotificacaoDto
+    {
+        public RelatorioNotificacaoDto()
+        {
+            Filtro = new FiltroNotificacaoDto();
+            DREs = new List<DreNotificacaoDto>();
+        }
+
+        public FiltroNotificacaoDto Filtro { get; set; }
+
+        public List<DreNotificacaoDto> DREs { get; set; }
+    }
+}
