10c94e2c1570cb55ac1839e9d64c7290e6ae1360
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-10 21:49:49 -0300

    29979 - Query de Comunicados

---
+++ src/SME.SR.Application/Queries/Comunicado/ObterDadosLeituraComunicados/ObterDadosLeituraComunicadosQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Application
+{
+    public class ObterDadosLeituraComunicadosQuery : IRequest<IEnumerable<LeituraComunicadoDto>>
+    {
+        public ObterDadosLeituraComunicadosQuery(FiltroRelatorioLeituraComunicadosDto filtro)
+        {
+            Filtro = filtro;
+        }
+
+        public FiltroRelatorioLeituraComunicadosDto Filtro { get; set; }
+    }
+}
