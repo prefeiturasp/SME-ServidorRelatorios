6cb67e5bb774dc6290e4f8f325da438db226a779
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-17 09:33:53 -0300

    30965 - Corrigido Título da Pagina

--- src/SME.SR.Infra/Utilitarios/VariaveisAmbiente.cs
+++ src/SME.SR.Infra/Utilitarios/VariaveisAmbiente.cs
@@ -5,7 +5,7 @@ namespace SME.SR.Infra
     public class VariaveisAmbiente
     {
 
-        public string ConnectionStringSgp => Environment.GetEnvironmentVariable("ConnectionStrings__SGP-Postgres");        
+        public string ConnectionStringSgp => Environment.GetEnvironmentVariable("ConnectionStrings__SGP-PostgresHml");        
         public string ConnectionStringEol => Environment.GetEnvironmentVariable("EolConnection");
         public string ConnectionStringApiEol => Environment.GetEnvironmentVariable("ApiEolConnection");
         public string ConnectionStringCoreSso => Environment.GetEnvironmentVariable("CoreSSOConnection");
