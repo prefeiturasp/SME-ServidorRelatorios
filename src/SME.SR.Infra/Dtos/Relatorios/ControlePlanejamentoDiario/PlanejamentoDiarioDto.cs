1047bea55b6a7213769d1471d3a25b4de88f5c08
William Manoel dos Santos <william.santos@amcom.com.br>
2020-12-18 06:43:55 -0300

    DTO's e Mock para montagem do relatório

---
+++ src/SME.SR.Infra/Dtos/Relatorios/ControlePlanejamentoDiario/PlanejamentoDiarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class PlanejamentoDiarioDto
+    {       
+        public DateTime DataAula { get; set; }
+        public long QuantidadeAulas { get; set; }
+        public string PlanejamentoRealizado { get; set; }
+        public DateTime DateRegistro { get; set; }
+        public string Usuario { get; set; }
+        public string SecoesPreenchidas { get; set; }
+        public string ObjetivosSelecionados { get; set; }
+        public string MeusObjetivosEspecificos { get; set; }
+        public string DesenvolvimentoAula { get; set; }        
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/ControlePlanejamentoDiario/PlanejamentoDiarioInfantilDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class PlanejamentoDiarioInfantilDto
+    {
+        public DateTime DataAula { get; set; }
+        public long QuantidadeAulas { get; set; }
+        public string PlanejamentoRealizado { get; set; }
+        public DateTime DateRegistro { get; set; }
+        public string Usuario { get; set; }
+        public string SecoesPreenchidas { get; set; }
+        public string Planejamento { get; set; }
+    }
+}
