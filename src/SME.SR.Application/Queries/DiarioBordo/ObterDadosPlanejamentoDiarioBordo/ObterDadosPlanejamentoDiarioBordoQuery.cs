b3951f16dae82973c8e96409516d4c673d5e1e53
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-28 02:41:53 -0300

    31126 - Criado Query de Planejamento Diario Bordo

---
+++ src/SME.SR.Application/Queries/DiarioBordo/ObterDadosPlanejamentoDiarioBordo/ObterDadosPlanejamentoDiarioBordoQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+
+namespace SME.SR.Application
+{
+    public class ObterDadosPlanejamentoDiarioBordoQuery : IRequest<IEnumerable<TurmaPlanejamentoDiarioDto>>
+    {
+
+        public ObterDadosPlanejamentoDiarioBordoQuery(FiltroRelatorioPlanejamentoDiario parametros)
+        {
+            Parametros = parametros;
+        }
+
+        public FiltroRelatorioPlanejamentoDiario Parametros { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/PlanejamentoDiario/AulaDiarioBordoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class AulaDiarioBordoDto
+    {
+        public string Turma { get; set; }
+        public int? Bimestre { get; set; }
+        public string ComponenteCurricular { get; set; }
+        public DateTime DataAula { get; set; }
+        public DateTime? DataPlanejamento { get; set; }
+        public string UsuarioRf { get; set; }
+        public string Usuario { get; set; }
+        public string Planejamento { get; set; }
+        public string Reflexoes { get; set; }
+        public long? DevolutivaId { get; set; }
+    }
+}
