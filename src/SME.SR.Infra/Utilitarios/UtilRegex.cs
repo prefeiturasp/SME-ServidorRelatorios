e79d96cc990f3f0eba993df2f14cdca3ff7974df
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2021-01-04 18:55:20 -0300

    31603 - Utilitário para regex de html criado

---
+++ src/SME.SR.Infra/Utilitarios/UtilRegex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Timers;
+
+namespace SME.SR.Infra.Utilitarios
+{
+    public static class UtilRegex
+    {
+        public static string RemoverTagsHtml(string texto)
+        {
+            texto = Regex.Replace(texto, @"<[^>]*>", String.Empty);
+            texto = Regex.Replace(texto, @"&nbsp;", " ").Trim();
+            return texto;
+        }
+
+        public static string RemoverTagsHtmlMidia(string texto)
+        {
+            texto = Regex.Replace(texto, @"<img[^>]*>", " [arquivo indisponível na impressão] ");
+            texto = Regex.Replace(texto, @"<iframe[^>]*>", " [arquivo indisponível na impressão] ");
+            return texto;
+        }
+    }
+}
