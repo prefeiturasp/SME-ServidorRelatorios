57400802de164798e2e430a5a8b72b3ebf09af24
William Manoel dos Santos <william.santos@amcom.com.br>
2021-04-27 13:13:10 -0300

    Testes Apanhado Geral

---
+++ testes/SME.SR.Testes.QueryHandlers/RelatorioAcompanhamentoImagensDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Testes.QueryHandlers
+{
+    class RelatorioAcompanhamentoImagensDto
+    {
+    }
+}
--- testes/SME.SR.Testes.QueryHandlers/RelatorioFaltasFrequenciaTeste.cs
+++ testes/SME.SR.Testes.QueryHandlers/RelatorioFaltasFrequenciaTeste.cs
@@ -20,5 +20,25 @@ namespace SME.SR.Testes.QueryHandlers
             //{
             //}
         }
+        [Fact]
+        public async Task TratarImagens()
+        {
+
+
+            var dto = new AcompanhamentoAprendizagemAlunoRetornoDto();
+            dto.ApanhadoGeral = @"<ul style='list-style-type: disc;'><li><span style='font-size: 48px;'><strong style=''><span style='background-color: rgb(0, 0, 255);'>Teste galante 44<img src='https://localhost:5001/Arquivos/Editor/4e0600ee-682f-407e-b693-d10769de0200.jpg' style='width: 360; height: 200'></span></strong></span></li></ul><p>De acordo com a imagem abaixo:</p><p><img src='https://localhost:5001/Arquivos/Editor/ed1af274-3ba3-47e2-98d4-5b6c1a44e238.jpg' style='width: 360; height: 200'><br></p><p><br></p><table style='width: 100%;'><br></table>";
+
+            var percursoFormatado = dto.PercusoTurmaFormatado();
+            //try
+            //{
+
+            //    var repositorio = new RelatorioFaltasFrequenciaRepository(new VariaveisAmbiente());
+            //    var dados = await repositorio.ObterFaltasFrequenciaPorAno(2020, "108800", "094668", Modalidade.Fundamental, new string[] { "3" }, new string[] { "1105" }, new int[] { 1 });
+            //}
+            //catch (System.Exception ex)
+            //{
+            //}
+        }
+
     }
 }
