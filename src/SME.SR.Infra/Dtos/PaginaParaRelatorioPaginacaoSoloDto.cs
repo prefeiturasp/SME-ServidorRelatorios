188b6c2ef37dd2bdf6a71c61443ea8f9876b29db
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-05-03 19:16:13 -0300

    Adicionando paginação por aluno

---
+++ src/SME.SR.Infra/Dtos/PaginaParaRelatorioPaginacaoSoloDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SME.SR.Infra.Dtos
+{
+    public class PaginaParaRelatorioPaginacaoSoloDto
+    {
+        public PaginaParaRelatorioPaginacaoSoloDto(string html, int pagina, int total)
+        {
+            Html = html;
+            Pagina = pagina;
+            Total = total;
+        }
+
+        public string Html { get; set; }
+        public int Pagina { get; set; }
+        public int Total { get; set; }
+    }
+}
