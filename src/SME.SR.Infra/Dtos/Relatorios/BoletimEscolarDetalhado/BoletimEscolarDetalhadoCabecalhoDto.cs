738c7e07df6fed8b125e451982573b8d1a3b97cc
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-05-31 11:39:53 -0300

    Mapeamento DTO Boletim Detalhado

---
+++ src/SME.SR.Infra/Dtos/Relatorios/BoletimEscolarDetalhado/BoletimEscolarDetalhadoCabecalhoDto.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace SME.SR.Infra
+{
+    public class BoletimEscolarDetalhadoCabecalhoDto
+    {
+        [JsonProperty("nomeDre")]
+        public string NomeDre { get; set; }
+
+        [JsonProperty("nomeUe")]
+        public string NomeUe { get; set; }
+
+        [JsonProperty("nomeTurma")]
+        public string NomeTurma { get; set; }
+
+        [JsonProperty("foto")]
+        public string Foto { get; set; }
+
+        [JsonProperty("aluno")]
+        public string Aluno { get; set; }
+
+        [JsonProperty("codigoEol")]
+        public string CodigoEol { get; set; }
+
+        [JsonProperty("data")]
+        public string Data { get; set; }
+
+        [JsonProperty("frequenciaGlobal")]
+        public string FrequenciaGlobal { get; set; }
+
+        [JsonProperty("ciclo")]
+        public string Ciclo { get; set; }
+    }
+}
