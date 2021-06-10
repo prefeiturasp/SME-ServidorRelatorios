9ffdd1472c9734a165f64c20315e2e279f8208cc
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-06-10 15:35:48 -0300

    merge release

--- src/SME.SR.Application/Commands/ComunsRelatorio/GerarRelatorioHtmlPDFBoletimDetalhadoCommand/GerarRelatorioHtmlPDFBoletimDetalhadoCommandHandler.cs
+++ src/SME.SR.Application/Commands/ComunsRelatorio/GerarRelatorioHtmlPDFBoletimDetalhadoCommand/GerarRelatorioHtmlPDFBoletimDetalhadoCommandHandler.cs
@@ -75,7 +75,7 @@ namespace SME.SR.Application.Commands.ComunsRelatorio.GerarRelatorioHtmlParaPdf
 
             if (request.EnvioPorRabbit)
             {
-                servicoFila.PublicaFila(new PublicaFilaDto(new MensagemRelatorioProntoDto(request.MensagemUsuario, request.MensagemTitulo), RotasRabbit.FilaSgp, RotasRabbit.RotaRelatoriosProntosSgp, RotasRabbit.ExchangeSgp, request.CodigoCorrelacao));
+                servicoFila.PublicaFila(new PublicaFilaDto(new MensagemRelatorioProntoDto(request.MensagemUsuario, request.MensagemTitulo), RotasRabbit.RotaRelatoriosProntosSgp, RotasRabbit.ExchangeSgp, request.CodigoCorrelacao));
                 return string.Empty;
             }
 
