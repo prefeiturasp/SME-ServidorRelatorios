0c047ce92dc6f4ce350352aef03859dcbb9082d1
Jeferson Cassiano <jeferson.nicolau92@gmail.com>
2021-07-01 11:42:19 -0300

    Ajuste ordenação componentes

--- src/SME.SR.Application/Queries/AcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamentoQuery.cs
+++ src/SME.SR.Application/Queries/AcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamentoQuery.cs
@@ -7,7 +7,7 @@ namespace SME.SR.Application
 {
     public class MontarRelatorioAcompanhamentoFechamentoQuery : IRequest<RelatorioAcompanhamentoFechamentoPorUeDto>
     {
-        public MontarRelatorioAcompanhamentoFechamentoQuery(Dre dre, Ue ue, string turmaCodigo, IEnumerable<Turma> turmas, IEnumerable<DisciplinaDto> componentesCurriculares, int[] bimestres, IEnumerable<FechamentoConsolidadoComponenteTurmaDto> consolidadoFechamento, IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> consolidadoConselhosClasse, bool listarPendencias, IEnumerable<PendenciaParaFechamentoConsolidadoDto> pendencias, Usuario usuario)
+        public MontarRelatorioAcompanhamentoFechamentoQuery(Dre dre, Ue ue, string turmaCodigo, IEnumerable<Turma> turmas, IEnumerable<ComponenteCurricularPorTurma> componentesCurriculares, int[] bimestres, IEnumerable<FechamentoConsolidadoComponenteTurmaDto> consolidadoFechamento, IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> consolidadoConselhosClasse, bool listarPendencias, IEnumerable<PendenciaParaFechamentoConsolidadoDto> pendencias, Usuario usuario)
         {
             Dre = dre;
             Ue = ue;
@@ -28,7 +28,7 @@ namespace SME.SR.Application
         public int[] Bimestres { get; set; }
         public IEnumerable<FechamentoConsolidadoComponenteTurmaDto> ConsolidadoFechamento { get; set; }
         public IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> ConsolidadoConselhosClasse { get; set; }
-        public IEnumerable<DisciplinaDto> ComponentesCurriculares { get; set; }
+        public IEnumerable<ComponenteCurricularPorTurma> ComponentesCurriculares { get; set; }
         public IEnumerable<PendenciaParaFechamentoConsolidadoDto> Pendencias { get; set; }
         public bool ListarPendencias { get; set; }
         public string TurmaCodigo { get; set; }
