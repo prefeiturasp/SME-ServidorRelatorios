cd82dc747ab22020ae8a216c377cc8dc4ba28d47
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-07-28 14:21:47 -0300

    classe não foi inserida

---
+++ src/SME.SR.Infra/Dtos/Relatorios/RegistroFrequencia/TurmaComponenteQtdAulasDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class TurmaComponenteQtdAulasDto
+    {
+        public string ComponenteCurricularCodigo { get; set; }
+        public string TurmaCodigo { get; set; }
+        public int AulasQuantidade { get; set; }
+        public int Bimestre { get; set; }
+    }
+}
