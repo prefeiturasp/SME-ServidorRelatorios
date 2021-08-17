ca537f29a0a652eb507a36923c92b3dda2309e14
thiiagofernando <thiago.fernando@msn.com>
2021-08-17 14:04:05 -0300

    Alterador de PendenciasFechamento para Pendencias

---
+++ src/SME.SR.Infra/Dtos/Relatorios/FechamentoPendencias/RelatorioPendenciasQueryRetornoDto.cs
@@ -0,0 +1,23 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioPendenciasQueryRetornoDto
+    {
+        public string Titulo { get; set; }
+        public string Detalhe { get; set; }
+        public int Situacao { get; set; }
+        public string DreNome { get; set; }
+        public string UeNome { get; set; }
+        public int AnoLetivo { get; set; }
+        public int ModalidadeCodigo { get; set; }
+        public string Semestre { get; set; }
+        public string TurmaNome { get; set; }
+        public string TurmaCodigo { get; set; }
+        public long DisciplinaId { get; set; }
+        public int Bimestre { get; set; }
+        public string Criador { get; set; }
+        public string CriadorRf { get; set; }
+        public string Aprovador { get; set; }
+        public string AprovadorRf { get; set; }
+        public string TipoPendencia { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/FechamentoPendencias/RelatorioPendenciasTurmaDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioPendenciasTurmaDto
+    {
+        public RelatorioPendenciasTurmaDto()
+        {
+            Bimestres = new List<RelatorioPendenciasBimestreDto>();
+        }
+        public string Nome { get; set; }
+        public List<RelatorioPendenciasBimestreDto> Bimestres { get; set; }
+
+    }
+}
