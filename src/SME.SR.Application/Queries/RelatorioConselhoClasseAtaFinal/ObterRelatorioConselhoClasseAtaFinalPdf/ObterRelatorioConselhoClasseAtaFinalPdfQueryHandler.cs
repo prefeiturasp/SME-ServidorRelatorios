a9d4ae24be681176990d2e9463b05ea2d9b96c72
William Manoel dos Santos <william.santos@amcom.com.br>
2021-08-19 09:50:05 -0300

    ajuste

--- src/SME.SR.Application/Queries/RelatorioConselhoClasseAtaFinal/ObterRelatorioConselhoClasseAtaFinalPdf/ObterRelatorioConselhoClasseAtaFinalPdfQueryHandler.cs
+++ src/SME.SR.Application/Queries/RelatorioConselhoClasseAtaFinal/ObterRelatorioConselhoClasseAtaFinalPdf/ObterRelatorioConselhoClasseAtaFinalPdfQueryHandler.cs
@@ -233,8 +233,7 @@ namespace SME.SR.Application
 
             var bimestres = periodosEscolares.Select(p => p.Bimestre).ToArray();
             var frequenciaAlunos = await ObterFrequenciaComponente(listaTurmas.ToArray(), componentesCurricularesPorTurma, bimestres, tipoCalendarioId);
-
-            //var frequenciaAlunos = Enumerable.Empty<FrequenciaAluno>();
+                        
             var areasDoConhecimento = await ObterAreasConhecimento(componentesDaTurma);
 
             var ordenacaoGrupoArea = await ObterOrdenacaoAreasConhecimento(componentesDaTurma, areasDoConhecimento);
