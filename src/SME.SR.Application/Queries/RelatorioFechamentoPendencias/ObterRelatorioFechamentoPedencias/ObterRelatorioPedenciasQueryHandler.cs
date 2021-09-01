3e9799a1dbe55e093131764f656d07db16677f70
William Manoel dos Santos <william.santos@amcom.com.br>
2021-09-01 00:22:07 -0300

    Ajuste

--- src/SME.SR.Application/Queries/RelatorioFechamentoPendencias/ObterRelatorioFechamentoPedencias/ObterRelatorioPedenciasQueryHandler.cs
+++ src/SME.SR.Application/Queries/RelatorioFechamentoPendencias/ObterRelatorioFechamentoPedencias/ObterRelatorioPedenciasQueryHandler.cs
@@ -223,7 +223,16 @@ namespace SME.SR.Application
 
                 if (exibirDetalhamento)
                 {
-                    pendenciaParaAdicionar.DetalhamentoPendencia = UtilRegex.RemoverTagsHtml(item.Detalhe);
+                    pendenciaParaAdicionar.Detalhes = new List<string>
+                                {
+                                    "Quantidade de Eventos de Conselho de Escola (8)",
+                                    "Quantidade de Eventos de Conselho de Classe (8)",
+                                    "Quantidade de Eventos de Conselho de Escola (8)",
+                                    "Quantidade de Eventos de Conselho de Classe (8)",
+                                    "Quantidade de Eventos de Conselho de Escola (8)",
+                                    "Quantidade de Eventos de Conselho de Escola (8)"
+                                };
+                    pendenciaParaAdicionar.DetalhamentoPendencia = "Eventos pendentes de cadastro:"; //UtilRegex.RemoverTagsHtml(item.Detalhe);
                     pendenciaParaAdicionar.DetalhamentoPendencia = pendenciaParaAdicionar.DetalhamentoPendencia.Replace("Clique aqui para acessar o plano.", "");
                     pendenciaParaAdicionar.DetalhamentoPendencia = pendenciaParaAdicionar.DetalhamentoPendencia.Replace("Clique aqui para acessar o plano e atribuir", "Para resolver esta pendência você precisa atribuir");
 
