844d8714a210977ed5d81a46b6126b08dcd51990
Thiago Fernando <thiago.fernando@msn.com>
2021-09-13 15:26:41 -0300

    Ajustes

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFechamentoConsolidadoPorUe/RelatorioAcompanhamentoFechamentoConsolidadoTurmaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoFechamentoConsolidadoTurmaDto
+    {
+        public RelatorioAcompanhamentoFechamentoConsolidadoTurmaDto(string turmaDescricao)
+        {
+            TurmaDescricao = turmaDescricao;
+            FechamentoConsolidadoBimestre = List<RelatorioAcompanhamentoFechamentoConsolidadoBimestreDto>();
+        }
+        public string TurmaDescricao { get; set; }
+        public List<RelatorioAcompanhamentoFechamentoConsolidadoBimestreDto> FechamentoConsolidadoBimestre { get; set; }
+    }
+}
