4d3def59e15c4fb8b3ec3f67e9ff08556974d0ca
Thiago Fernando <thiago.fernando@msn.com>
2021-09-13 18:18:50 -0300

    Ajustes ignorado

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFechamentoConsolidadoPorUe/RelatorioAcompanhamentoFechamentoConsolidadoUesDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoFechamentoConsolidadoUesDto
+    {
+        public RelatorioAcompanhamentoFechamentoConsolidadoUesDto()
+        {
+            FechamentoConselhoClasseConsolidado = new List<RelatorioAcompanhamentoFechamentoConselhoClasseConsolidadoDto>();
+        }
+
+        public string NomeTurma { get; set; }
+        public string NomeUe { get; set; }
+        public string NomeBimestre { get; set; }
+        //remover este comentario
+        //remover este comentario
+        //remover este comentario
+        //remover este comentario
+        //remover este comentario
+        //remover este comentario
+        public List<RelatorioAcompanhamentoFechamentoConselhoClasseConsolidadoDto> FechamentoConselhoClasseConsolidado { get; set; }
+    }
+}
