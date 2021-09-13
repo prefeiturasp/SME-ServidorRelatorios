c286cd2d80dd92b49b598224cd20bb9d37f36f69
Thiago Fernando <thiago.fernando@msn.com>
2021-09-13 17:25:52 -0300

    Ajustes

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFechamentoConsolidadoPorUe/RelatorioAcompanhamentoConselhoClasseConsolidadoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoConselhoClasseConsolidadoDto
+    {
+        public string NaoIniciado { get; set; }
+        public string EmAndamento { get; set; }
+        public string Concluido { get; set; }
+    }
+}
