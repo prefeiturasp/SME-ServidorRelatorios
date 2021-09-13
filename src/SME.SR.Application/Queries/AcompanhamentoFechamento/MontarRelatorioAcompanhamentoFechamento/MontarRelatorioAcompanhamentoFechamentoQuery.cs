79139ca586d65f6ecea3c5850c91e2e3d12c5d14
Thiago Fernando <thiago.fernando@msn.com>
2021-09-13 17:54:23 -0300

    Ajustes no nome

--- src/SME.SR.Application/Queries/AcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamentoQuery.cs
+++ src/SME.SR.Application/Queries/AcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamento/MontarRelatorioAcompanhamentoFechamentoQuery.cs
@@ -7,7 +7,7 @@ namespace SME.SR.Application
 {
     public class MontarRelatorioAcompanhamentoFechamentoQuery : IRequest<RelatorioAcompanhamentoFechamentoPorUeDto>
     {
-        public MontarRelatorioAcompanhamentoFechamentoQuery(Dre dre, Ue ue, string[] turmasCodigo, IEnumerable<Turma> turmas, IEnumerable<ComponenteCurricularPorTurma> componentesCurriculares, int[] bimestres, IEnumerable<FechamentoConsolidadoComponenteTurmaDto> consolidadoFechamento, IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> consolidadoConselhosClasse, bool listarPendencias, IEnumerable<PendenciaParaFechamentoConsolidadoDto> pendencias, Usuario usuario)
+        public MontarRelatorioAcompanhamentoFechamentoQuery(Dre dre, Ue ue, string[] turmasCodigo, IEnumerable<Turma> turmas, IEnumerable<ComponenteCurricularPorTurma> componentesCurriculares, int[] bimestres, IEnumerable<FechamentoConsolidadoComponenteTurmaDto> consolidadoFechamento, IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> consolidadoConselhosClasse, bool listarPendencias, IEnumerable<PendenciaParaFechamentoConsolidadoDto> pendencias, Usuario usuario, bool todasUes, IEnumerable<FechamentoConsolidadoComponenteTurmaDto> consolidadoFechamentoTodasUes, IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> consolidadoConselhosClasseTodasUes)
         {
             Dre = dre;
             Ue = ue;
@@ -20,6 +20,9 @@ namespace SME.SR.Application
             Pendencias = pendencias;
             ListarPendencias = listarPendencias;
             Usuario = usuario;
+            TodasUes = todasUes;
+            ConsolidadoFechamentoTodasUes = consolidadoFechamentoTodasUes;
+            ConsolidadoConselhosClasseTodasUes = consolidadoConselhosClasseTodasUes;
         }
 
         public Dre Dre { get; set; }
@@ -28,10 +31,13 @@ namespace SME.SR.Application
         public int[] Bimestres { get; set; }
         public IEnumerable<FechamentoConsolidadoComponenteTurmaDto> ConsolidadoFechamento { get; set; }
         public IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> ConsolidadoConselhosClasse { get; set; }
+        public IEnumerable<FechamentoConsolidadoComponenteTurmaDto> ConsolidadoFechamentoTodasUes { get; set; }
+        public IEnumerable<ConselhoClasseConsolidadoTurmaAlunoDto> ConsolidadoConselhosClasseTodasUes { get; set; }
         public IEnumerable<ComponenteCurricularPorTurma> ComponentesCurriculares { get; set; }
         public IEnumerable<PendenciaParaFechamentoConsolidadoDto> Pendencias { get; set; }
         public bool ListarPendencias { get; set; }
         public string[] TurmasCodigo { get; set; }
         public Usuario Usuario { get; set; }
+        public bool TodasUes { get; set; }
     }
 }
