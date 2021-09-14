29c648098c00df9a6f987401b64595a671ade7d7
wagnersantos-amcom <wagner.santos@amcom.com.br>
2021-09-14 12:03:32 -0300

    Ajustes DTO e mock

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFechamentoConsolidadoPorUe/RelatorioAcompanhamentoFechamentoConselhoClasseConsolidadoDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoFechamentoConselhoClasseConsolidadoDto
+    {
+        public RelatorioAcompanhamentoFechamentoConselhoClasseConsolidadoDto(string nomeTurma)
+        {
+            NomeTurma = nomeTurma;
+            FechamentoConsolidado = new List<RelatorioAcompanhamentoFechamentoConsolidadoDto>();
+            ConselhoDeClasseConsolidado = new List<RelatorioAcompanhamentoConselhoClasseConsolidadoDto>();
+        }
+
+        public string NomeTurma { get; set; }
+        public List<RelatorioAcompanhamentoFechamentoConsolidadoDto> FechamentoConsolidado { get; set; }
+        public List<RelatorioAcompanhamentoConselhoClasseConsolidadoDto> ConselhoDeClasseConsolidado { get; set; }
+    }
+}
