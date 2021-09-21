cf3235c4fb197a0b2c2bf06fbcaf094a87a78d3c
William Manoel dos Santos <william.santos@amcom.com.br>
2021-09-21 09:42:33 -0300

    Ajuste dto

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFechamentoUe/RelatorioAcompanhamentoFechamentoUesDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoFechamentoUesDto
+    {
+        public RelatorioAcompanhamentoFechamentoUesDto()
+        {            
+            Bimestres = new List<RelatorioAcompanhamentoFechamentoBimestresDto>();
+        }
+
+        public string NomeUe { get; set; }
+        public List<RelatorioAcompanhamentoFechamentoBimestresDto> Bimestres { get; set; }
+    }
+}
