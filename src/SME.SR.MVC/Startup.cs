8a3aeb0fa4699d85a839de2aa3f625b84f345f95
elitonds <elitonds@gmail.com>
2021-10-04 14:51:52 -0300

    ajustes

--- src/SME.SR.MVC/Startup.cs
+++ src/SME.SR.MVC/Startup.cs
@@ -27,6 +27,7 @@ namespace SME.SR.MVC
             {
                 c.Filters.Add(new ExcecaoMiddleware(Configuration));
             });
+            services.AddPolicies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
