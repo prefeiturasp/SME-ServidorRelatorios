a93c03e98c40dfdca410678bd34ee39746d2bb7b
Helio Barros <helio.barros@amcom.com.br>
2021-10-06 15:00:23 -0300

    ajuste dto

---
+++ src/SME.SR.Infra/Dtos/Relatorios/NotasEConceitosFinais/NotaConceitoEmAprovacaoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace SME.SR.Infra.Dtos.Relatorios.NotasEConceitosFinais
+{
+    public class NotaConceitoEmAprovacaoDto
+    {
+        public long WorkflowId { get; set; }
+        public long? ConceitoId { get; set; }
+        public string Conceito { get; set; }
+        public double? Nota { get; set; }
+        public string NotaConceito { get => ConceitoId.HasValue ? Conceito : Nota.HasValue ? Nota.Value.ToString("0.0", CultureInfo.InvariantCulture) : ""; }
+    }
+}
