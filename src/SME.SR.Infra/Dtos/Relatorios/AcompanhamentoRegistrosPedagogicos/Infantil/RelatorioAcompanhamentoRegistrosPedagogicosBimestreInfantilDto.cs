56e27893821e68a26548df2c567ccc8325fd3124
Helio Barros <helio.barros@amcom.com.br>
2021-10-11 11:14:59 -0300

    Ajustes DTO's + UseCase para emitir relatório + Início das Querys de consolidação de turmas

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoRegistrosPedagogicos/Infantil/RelatorioAcompanhamentoRegistrosPedagogicosBimestreInfantilDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioAcompanhamentoRegistrosPedagogicosBimestreInfantilDto
+    {
+        public int Bimestre { get; set; }
+        public List<RelatorioAcompanhamentRegistrosPedagogicosTurmaInfantilDto> TurmasInfantil { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoRegistrosPedagogicos/Infantil/RelatorioAcompanhamentoRegistrosPedagogicosInfantilDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+    public class RelatorioAcompanhamentoRegistrosPedagogicosInfantilDto
+    {
+        public RelatorioAcompanhamentoRegistrosPedagogicosInfantilDto()
+        {
+            Bimestre = new List<RelatorioAcompanhamentoRegistrosPedagogicosBimestreDto>();
+        }
+        public RelatorioAcompanhamentoRegistrosPedagogicosCabecalhoDto Cabecalho { get; set; }
+        public List<RelatorioAcompanhamentoRegistrosPedagogicosBimestreDto> Bimestre { get; set; }
+    }
+}
