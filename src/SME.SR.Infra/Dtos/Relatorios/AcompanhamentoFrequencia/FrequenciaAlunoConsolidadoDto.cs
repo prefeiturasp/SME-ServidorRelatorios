3051b0b33ab54c429113792b5afbb0ff01f37cd4
Thiago Fernando <thiago.fernando@msn.com>
2021-11-04 16:18:46 -0300

    Query de consulta dos dados de Frequência

---
+++ src/SME.SR.Infra/Dtos/Relatorios/AcompanhamentoFrequencia/FrequenciaAlunoConsolidadoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SME.SR.Infra.Dtos.Relatorios.AcompanhamentoFrequencia
+{
+    public class FrequenciaAlunoConsolidadoDto
+    {
+        public string Bimestre { get; set; }
+        public string TotalAula { get; set; }
+        public string TotalPresencas { get; set; }
+        public string TotalRemotos { get; set; }
+        public string TotalAusencias { get; set; }
+        public string TotalCompensacoes { get; set; }
+        public string CodigoAluno { get; set; }
+    }
+}
