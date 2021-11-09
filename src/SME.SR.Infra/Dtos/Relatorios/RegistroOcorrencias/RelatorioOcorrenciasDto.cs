55101ccb085f315706a94aee1d2b090b0463e8cf
wagnersantos-amcom <wagner.santos@amcom.com.br>
2021-11-09 18:55:51 -0300

    Leiaute Html

--- src/SME.SR.Infra/Dtos/Relatorios/RegistroOcorrencias/RelatorioOcorrenciasDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/RegistroOcorrencias/RelatorioOcorrenciasDto.cs
@@ -6,7 +6,9 @@ namespace SME.SR.Infra
 {
     public class RelatorioOcorrenciasDto
     {
+        public string CriancaNome { get; set; }
         public string DataOcorrencia { get; set; }
+        public string Turma { get; set; }
         public string TipoOcorrencia { get; set; }
         public string TituloOcorrencia { get; set; }
         public string DescricaoOcorrencia { get; set; }        
