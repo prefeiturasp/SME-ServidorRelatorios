603184dc0fe7f51c2bcc50f1c7b43de59e1843c4
Luiz Cezário <lkcezario@gmail.com>
2021-11-19 15:11:53 -0300

    Adiciona FiltroImpressaoOcorrenciaDto

---
+++ src/SME.SR.Infra/Dtos/Relatorios/Ocorrencia/FiltroImpressaoOcorrenciaDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra{
+    public class FiltroImpressaoOcorrenciaDto
+    {
+        public string DreCodigo { get; set; }
+        public string UeCodigo { get; set; }
+        public string TurmaCodigo { get; set; }
+        public string UsuarioNome { get; set; }
+        public string UsuarioRf { get; set; }
+        public IEnumerable<long> OcorrenciasIds { get; set; }
+    }
+}
