37d77d04810ea15d2bf7406833ae3e47b37fd30e
Rafael Losi <rafael.losi@amcom.com.br>
2021-11-22 11:41:39 -0300

    26880 Paginação por Aluno

---
+++ src/SME.SR.Application/Commands/ComunsRelatorio/GerarRelatorioOcorrencias/GerarRelatorioOcorrenciasCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System;
+
+namespace SME.SR.Application
+{
+    public class GerarRelatorioOcorrenciasCommand : IRequest
+    {
+
+        public GerarRelatorioOcorrenciasCommand(RelatorioRegistroOcorrenciasDto relatorio, Guid codigoCorrelacao)
+        {
+            Relatorio = relatorio;
+            CodigoCorrelacao = codigoCorrelacao;
+        }
+
+        public RelatorioRegistroOcorrenciasDto Relatorio { get; }
+        public Guid CodigoCorrelacao { get; }
+    }
+}
