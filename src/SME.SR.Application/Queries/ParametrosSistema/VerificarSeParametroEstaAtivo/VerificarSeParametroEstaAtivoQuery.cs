bf8aa69e52578a013349d852a2dd8ff0cc9d6a7d
Thiago Fernando <thiago.fernando@msn.com>
2022-01-07 11:05:52 -0300

    Implementado parametro para utilização de novo Layout

---
+++ src/SME.SR.Application/Queries/ParametrosSistema/VerificarSeParametroEstaAtivo/VerificarSeParametroEstaAtivoQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SR.Infra;
+
+namespace SME.SR.Application
+{
+    public class VerificarSeParametroEstaAtivoQuery : IRequest<ParametroSistemaAnoSituacaoDto>
+    {
+        public VerificarSeParametroEstaAtivoQuery(TipoParametroSistema tipoParametro)
+        {
+            TipoParametro = tipoParametro;
+        }
+
+        public TipoParametroSistema TipoParametro { get; set; }
+    }
+}
---
+++ src/SME.SR.Application/Queries/ParametrosSistema/VerificarSeParametroEstaAtivo/VerificarSeParametroEstaAtivoQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SR.Data.Interfaces;
+using SME.SR.Infra;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class VerificarSeParametroEstaAtivoQueryHandler : IRequestHandler<VerificarSeParametroEstaAtivoQuery, ParametroSistemaAnoSituacaoDto>
+    {
+        private readonly IParametroSistemaRepository parametroSistemaRepository;
+        public VerificarSeParametroEstaAtivoQueryHandler(IParametroSistemaRepository parametroSistemaRepository)
+        {
+            this.parametroSistemaRepository = parametroSistemaRepository ?? throw new ArgumentNullException(nameof(parametroSistemaRepository));
+        }
+
+        public async Task<ParametroSistemaAnoSituacaoDto> Handle(VerificarSeParametroEstaAtivoQuery request, CancellationToken cancellationToken)
+        {
+            return await parametroSistemaRepository.VerificarSeParametroEstaAtivo(request.TipoParametro);
+        }
+    }
+}
