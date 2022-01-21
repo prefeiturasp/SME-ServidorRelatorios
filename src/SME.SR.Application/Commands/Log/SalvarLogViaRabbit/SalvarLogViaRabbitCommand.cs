5f4c8aa7c904416bdbada5ec28434615a09685d2
Rafael Losi <rafael.losi@amcom.com.br>
2022-01-21 14:31:33 -0300

    Incluso Command de Log no Elastick Stack

---
+++ src/SME.SR.Application/Commands/Log/SalvarLogViaRabbit/SalvarLogViaRabbitCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Newtonsoft.Json.Converters;
+using SME.SR.Infra;
+using System.Text.Json.Serialization;
+
+namespace SME.SR.Application
+{
+    public class SalvarLogViaRabbitCommand : IRequest
+    {
+        public SalvarLogViaRabbitCommand(string mensagem, LogNivel nivel, string observacao = "")
+        {
+            Mensagem = mensagem;
+            Nivel = nivel;
+            Observacao = observacao;
+        }
+
+        public string Mensagem { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public LogNivel Nivel { get; set; }
+        public string Observacao { get; set; }
+    }
+}
---
+++ src/SME.SR.Application/Commands/Log/SalvarLogViaRabbit/SalvarLogViaRabbitCommandHandler.cs
@@ -0,0 +1,77 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using SME.SR.Infra;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class SalvarLogViaRabbitCommandHandler : AsyncRequestHandler<SalvarLogViaRabbitCommand>
+    {
+        private readonly ConfiguracaoRabbitLogOptions configuracaoRabbitOptions;
+        public SalvarLogViaRabbitCommandHandler(ConfiguracaoRabbitLogOptions configuracaoRabbitOptions)
+        {
+            this.configuracaoRabbitOptions = configuracaoRabbitOptions ?? throw new ArgumentNullException(nameof(configuracaoRabbitOptions));
+        }
+
+        protected override Task Handle(SalvarLogViaRabbitCommand request, CancellationToken cancellationToken)
+        {
+            var mensagem = JsonConvert.SerializeObject(new LogMensagem(request.Mensagem, request.Nivel.ToString(), "Relatorios", request.Observacao, "SR"), new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+
+            var body = Encoding.UTF8.GetBytes(mensagem);
+
+            PublicarMensagem(body);
+
+            return Task.CompletedTask;
+        }
+
+        private void PublicarMensagem(byte[] body)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = configuracaoRabbitOptions.HostName,
+                UserName = configuracaoRabbitOptions.UserName,
+                Password = configuracaoRabbitOptions.Password,
+                VirtualHost = configuracaoRabbitOptions.VirtualHost
+            };
+
+            using (var conexaoRabbit = factory.CreateConnection())
+            {
+                using (IModel _channel = conexaoRabbit.CreateModel())
+                {
+                    var props = _channel.CreateBasicProperties();
+
+                    //_channel.QueueBind(RotasRabbitSGP.RotaLogs, ExchangeRabbit.SgpLogs, RotasRabbitSGP.RotaLogs);
+                    _channel.BasicPublish(ExchangeRabbit.SgpLogs, RotasRabbitSGP.RotaLogs, props, body);
+                }
+            }
+        }
+
+        public class LogMensagem
+        {
+            public LogMensagem(string mensagem, string nivel, string contexto, string observacao, string projeto)
+            {
+                Mensagem = mensagem;
+                Nivel = nivel;
+                Contexto = contexto;
+                Observacao = observacao;
+                Projeto = projeto;
+                DataHora = DateTime.Now;
+            }
+
+            public string Mensagem { get; set; }
+            public string Nivel { get; set; }
+            public string Contexto { get; set; }
+            public string Observacao { get; set; }
+            public string Projeto { get; set; }
+            public DateTime DataHora { get; set; }
+
+        }
+    }
+}
