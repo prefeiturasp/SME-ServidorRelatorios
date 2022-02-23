59963a7e497c7bb6e2bfc5a6823984a8ca5038c6
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2022-02-23 18:26:50 -0300

    Evitando conflitos

--- src/SME.SR.Data/Repositories/Sgp/UeRepository.cs
+++ src/SME.SR.Data/Repositories/Sgp/UeRepository.cs
@@ -94,7 +94,7 @@ namespace SME.SR.Data
 
         public async Task<Ue> ObterUeComDrePorId(long ueId)
         {
-            var query = @"select ue.*, dre.*
+            var query = @"select ue.*, dre.* 
                             from ue 
                            inner join dre on dre.id = ue.dre_id
                            where ue.id = @ueId";
