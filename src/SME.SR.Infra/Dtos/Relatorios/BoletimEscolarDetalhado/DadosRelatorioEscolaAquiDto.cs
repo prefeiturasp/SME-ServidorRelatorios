36ad82a5e5c287de756d45fa36ee6de64cb5af24
Thiago Fernando <thiago.fernando@msn.com>
2022-03-18 13:06:04 -0300

    Criar endpoint (SR) para processar RAA do APP

---
+++ src/SME.SR.Infra/Dtos/Relatorios/BoletimEscolarDetalhado/DadosRelatorioEscolaAquiDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SME.SR.Infra
+{
+   public class DadosRelatorioEscolaAquiDto
+    {
+        public DadosRelatorioEscolaAquiDto(BoletimEscolarDetalhadoDto boletimEscolarDetalhado)
+        {
+            this.BoletimEscolarDetalhado = boletimEscolarDetalhado;
+        }
+
+        public BoletimEscolarDetalhadoDto BoletimEscolarDetalhado { get; set; }
+    }
+}
