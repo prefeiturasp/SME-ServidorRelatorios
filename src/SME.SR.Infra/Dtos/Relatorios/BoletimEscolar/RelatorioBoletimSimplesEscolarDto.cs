2cb4f591441740f041a00109787099ac60b3f2a0
Helio Barros <helio.barros@amcom.com.br>
2022-03-23 08:38:30 -0300

    dto estava ignorado

---
+++ src/SME.SR.Infra/Dtos/Relatorios/BoletimEscolar/RelatorioBoletimSimplesEscolarDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioBoletimSimplesEscolarDto
+    {
+        public BoletimEscolarCabecalhoDto Cabecalho { get; set; }
+
+        public string ParecerConclusivo { get; set; }
+
+        public ComponenteCurricularRegenciaDto ComponenteCurricularRegencia { get; set; }
+
+        public List<ComponenteCurricularDto> ComponentesCurriculares { get; set; }
+
+        public RelatorioBoletimSimplesEscolarDto()
+        {
+            Cabecalho = new BoletimEscolarCabecalhoDto();
+        }
+    }
+}
