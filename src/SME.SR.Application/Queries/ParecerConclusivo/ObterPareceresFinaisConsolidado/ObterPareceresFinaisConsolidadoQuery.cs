b19bc04e1ff66c60266e5f5aa1d39f1d2dbab1aa
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2022-03-28 16:44:13 -0300

    Correções do parecer conclusivo

---
+++ src/SME.SR.Application/Queries/ParecerConclusivo/ObterPareceresFinaisConsolidado/ObterPareceresFinaisConsolidadoQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+
+namespace SME.SR.Application
+{
+    public class ObterPareceresFinaisConsolidadoQuery : IRequest<IEnumerable<RelatorioParecerConclusivoRetornoDto>>
+    {
+        public int AnoLetivo { get; set; }
+        public string DreCodigo { get; set; }
+        public string UeCodigo { get; set; }
+        public Modalidade? Modalidade { get; set; }
+        public int? Semestre { get; set; }
+        public string[] TurmasCodigo { get; set; }
+        public string[] Anos { get; set; }
+        public long ParecerConclusivoId { get; set; }
+    }
+}
