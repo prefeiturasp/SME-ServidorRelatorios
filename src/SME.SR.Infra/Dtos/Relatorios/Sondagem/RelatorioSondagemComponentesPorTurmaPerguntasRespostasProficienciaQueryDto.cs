c5a6599b12ebb872e4ea2d148f3b6e91d22c9348
Fernando Groeler <fernando.groeler@gmail.com>
2022-04-07 13:08:19 -0400

    Adicionado DTO que não foi incluido no commit.

---
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioSondagemComponentesPorTurmaPerguntasRespostasProficienciaQueryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SME.SR.Infra
+{
+    public class RelatorioSondagemComponentesPorTurmaPerguntasRespostasProficienciaQueryDto : RelatorioSondagemComponentesPorTurmaPerguntasRespostasQueryDto
+    {
+        public string SubPergunta { get; set; }
+    }
+}
