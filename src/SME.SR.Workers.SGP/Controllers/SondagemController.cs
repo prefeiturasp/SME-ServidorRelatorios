ed408800ffeeba681fb0e80605c38a6a90c52424
DanylloAntunes <danyllo.antunes@gmail.com>
2022-04-07 17:35:23 -0300

    voltando anotação key

--- src/SME.SR.Workers.SGP/Controllers/SondagemController.cs
+++ src/SME.SR.Workers.SGP/Controllers/SondagemController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace SME.SR.Workers.SGP.Controllers
 {
     [ApiController]
+    [ChaveIntegracaoSrApi]
     [Route("api/v1/[controller]")]
     public class SondagemController : ControllerBase
     {
