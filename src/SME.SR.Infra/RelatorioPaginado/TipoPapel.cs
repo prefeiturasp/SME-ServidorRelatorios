27982d7d8ee9904e8f96afd3d7ff947547ff4645
DanylloAntunes <danyllo.antunes@gmail.com>
2022-04-07 09:45:00 -0300

    Inclusão de itens pendentes

---
+++ src/SME.SR.Infra/RelatorioPaginado/TipoPapel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra.RelatorioPaginado
+{
+    public sealed class TipoPapel
+    {
+        public static readonly TipoPapel A4 = new TipoPapel(EnumTipoDePapel.A4, 1120, 790);
+        public static readonly TipoPapel A4_Retrato = new TipoPapel(EnumTipoDePapel.A4_RETRATO, 790, 1120);
+
+        private static readonly List<TipoPapel> lista = new List<TipoPapel>() { A4, A4_Retrato };
+
+        public EnumTipoDePapel TipoDePapel { get; private set; }
+
+        public int AlturaPx { get; private set; }
+
+        public int LarguraPx { get; private set; }
+
+        public TipoPapel ObtenhaTipo(EnumTipoDePapel enumTipo)
+        {
+            return lista.Find(x => x.TipoDePapel == enumTipo);
+        }
+
+        private TipoPapel(EnumTipoDePapel tipo, int altura, int largura)
+        {
+            this.TipoDePapel = tipo;
+            this.AlturaPx = altura;
+            this.LarguraPx = largura;
+        }
+    }
+}
