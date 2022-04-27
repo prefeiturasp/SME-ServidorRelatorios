0d42293035706473f8b9d8b3a3eb161c9915f41f
Fernando Groeler <fernando.groeler@gmail.com>
2022-04-27 07:29:38 -0400

    Adicionado arquivo ignorado ao projeto.

---
+++ src/SME.SR.Infra/Dtos/Relatorios/FrequenciaGlobal/FrequenciaAlunoMensalConsolidadoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SME.SR.Infra
+{
+    public class FrequenciaAlunoMensalConsolidadoDto
+    {
+        public string DreSigla { get; set; }
+        public string UeNome { get; set; }
+        public string DescricaoTipoEscola { get; set; }
+        public int Mes { get; set; }
+        public int ModalidadeCodigo { get; set; }
+        public string TurmaNome { get; set; }
+        public string CodigoEol { get; set; }
+        public decimal Percentual { get; set; }
+    }
+}
