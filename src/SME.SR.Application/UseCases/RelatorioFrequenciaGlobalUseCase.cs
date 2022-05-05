a91924e0feec5a858bf8b3572a164267290dcf2e
Thiago Fernando <thiago.fernando@msn.com>
2022-05-05 16:38:22 -0300

    Criado PreparadorDeRelatorioPaginadoFrequenciaGlobalMensal

--- src/SME.SR.Application/UseCases/RelatorioFrequenciaGlobalUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioFrequenciaGlobalUseCase.cs
@@ -44,146 +44,57 @@ namespace SME.SR.Application
 
         private async Task GerarRelatorioPdf(List<FrequenciaGlobalDto> listaDeFrequencia, FiltroRelatorioDto request, FiltroFrequenciaGlobalDto filtroRelatorio)
         {
-            var dto = new FrequenciaMensalDto();
-            await MapearCabecalho(dto, filtroRelatorio);
-            MapearFrequenciaMensalDre(listaDeFrequencia.GroupBy(x => x.DreCodigo).Distinct().ToList(), dto);
-            MapearFrequenciaMensalUe(listaDeFrequencia.GroupBy(x => x.UeCodigo).Distinct().ToList(), dto.Dres);
-            MapearFrequenciaMensalUeTurma(listaDeFrequencia.GroupBy(x => x.TurmaCodigo).Distinct().ToList(), dto.Dres);
-            MapearFrequenciaMensalUeTurmaMes(listaDeFrequencia.GroupBy(x => x.TurmaCodigo).Distinct().ToList(),dto.Dres);
-            MapearFrequenciaUesTurmaMesEstudande(listaDeFrequencia.GroupBy(x => x.CodigoEOL).Distinct().ToList(), dto.Dres);
-            
-            await mediator.Send(new GerarRelatorioHtmlParaPdfCommand("RelatorioFrequenciaGlobal", dto, request.CodigoCorrelacao));
+            var dto = new List<FrequenciaMensalDto>();
+            var cabecalho = new FrequenciaMensalCabecalhoDto();
+            await MapearCabecalho(cabecalho, filtroRelatorio);
+            MaperarDto(dto, listaDeFrequencia);
+            await ObtenhaRelatorioPaginado(dto, cabecalho, request.CodigoCorrelacao);
 
         }
-
-        private void MapearFrequenciaUesTurmaMesEstudande(List<IGrouping<string, FrequenciaGlobalDto>> listaDeFrequenciaUesTurmaMesEstudande, List<FrequenciaMensalDreDto> frequenciaMensalDre)
+        private void MaperarDto(List<FrequenciaMensalDto> dto, List<FrequenciaGlobalDto> listaDeFrequencia)
         {
-            var listaEstudantes = new List<FrequenciaMensalAlunoDto>();
-
-            foreach (var itemEstudante in listaDeFrequenciaUesTurmaMesEstudande)
+            foreach (var item in listaDeFrequencia)
             {
-                var estudante = new FrequenciaMensalAlunoDto 
+                var frequencia = new FrequenciaMensalDto
                 {
-                    CodigoAluno = itemEstudante.Key,
-                    NumeroAluno = itemEstudante.FirstOrDefault().NumeroChamadda,
-                    NomeAluno = itemEstudante.FirstOrDefault().Estudante,
-                    ProcentagemFrequencia = itemEstudante.FirstOrDefault().PercentualFrequencia,
-                    ValorMes = itemEstudante.FirstOrDefault().Mes,
-                    CodigoUe = itemEstudante.FirstOrDefault().UeCodigo,
-                    TurmaCodigo = itemEstudante.FirstOrDefault().TurmaCodigo
+                    CodigoDre = item.DreCodigo,
+                    NomeDre = item.SiglaDre,
+                    CodigoUe = item.UeCodigo,
+                    NomeUe = item.UeNome,
+                    NomeTurma = item.Turma,
+                    CodigoTurma = item.TurmaCodigo,
+                    NomeMes  = ObterNomeMesReferencia(item.Mes),
+                    ValorMes = item.Mes,
+                    CodigoAluno = item.CodigoEOL,
+                    NumeroAluno = item.NumeroChamadda,
+                    NomeAluno = item.Estudante,
+                    ProcentagemFrequencia = item.PercentualFrequencia
                 };
-                listaEstudantes.Add(estudante);
-            }
-            foreach (var itemDre in frequenciaMensalDre)
-            {
-                foreach (var itemUe in itemDre.Ues)
-                {
-                    foreach (var itemTurma in itemUe.Turmas)
-                    {
-                        foreach (var itemMes in itemTurma.Meses)
-                        {
-                            itemMes.Alunos.AddRange(listaEstudantes.Where(x => x.TurmaCodigo == itemMes.TurmaCodigo &&
-                                                                                         x.ValorMes == itemMes.ValorMes && x.CodigoUe == itemTurma.CodigoUe).OrderBy(x => x.NomeAluno));
-                        }
-                    }
-                }
+                dto.Add(frequencia);
             }
         }
-
-        private void MapearFrequenciaMensalUeTurmaMes(List<IGrouping<string, FrequenciaGlobalDto>> listaDeFrequenciaUesTurmaMes, List<FrequenciaMensalDreDto> frequenciaMensalDre)
+        private async Task<string> ObtenhaRelatorioPaginado(List<FrequenciaMensalDto> dto, FrequenciaMensalCabecalhoDto cabecalho, Guid codigoCorrelacao)
         {
-            var listaMes = new List<FrequenciaMensalMesDto>();
-            foreach (var itemMes in listaDeFrequenciaUesTurmaMes)
-            {
-                var freqMes = new FrequenciaMensalMesDto
-                {
-                    NomeMes = ObterNomeMesReferencia(itemMes.FirstOrDefault().Mes),
-                    ValorMes = itemMes.FirstOrDefault().Mes,
-                    TurmaCodigo = itemMes.FirstOrDefault().TurmaCodigo
-                };
-                listaMes.Add(freqMes);
-            }
+            var preparo = new PreparadorDeRelatorioPaginadoFrequenciaGlobalMensal(dto, cabecalho);
+            var dtoPaginado = preparo.ObtenhaRelatorioPaginadoDto();
 
-            foreach (var itemDre in frequenciaMensalDre)
-            {
-                foreach (var itemUe in itemDre.Ues)
-                {
-                    foreach (var itemTurma in itemUe.Turmas)
-                    {
-                        itemTurma.Meses.AddRange(listaMes.Where(x => x.TurmaCodigo == itemTurma.CodigoTurma).OrderBy(x => x.ValorMes));
-                    }
-                }
-            }
+            return await mediator.Send(new GerarRelatorioHtmlParaPdfCommand("RelatorioPaginado/Index", dtoPaginado, codigoCorrelacao));
         }
-
-        private void MapearFrequenciaMensalUeTurma(List<IGrouping<string, FrequenciaGlobalDto>> listaDeFrequenciaUesTurma, List<FrequenciaMensalDreDto> frequenciaMensalDre)
+        private async Task MapearCabecalho(FrequenciaMensalCabecalhoDto cabecalhoDto, FiltroFrequenciaGlobalDto filtroRelatorio)
         {
-            var listaTurmas = new List<FrequenciaMensalTurmaDto>();
-            foreach (var turma in listaDeFrequenciaUesTurma)
-            {
-                var freqTurma = new FrequenciaMensalTurmaDto
-                {
-                    NomeTurma = turma.FirstOrDefault().Turma,
-                    CodigoTurma = turma.Key,
-                    CodigoUe = turma.FirstOrDefault().UeCodigo,
-                };
-                listaTurmas.Add(freqTurma);
-            }
-            foreach (var itemDre in frequenciaMensalDre)
-            {
-                foreach (var itemUe in itemDre.Ues)
-                {
-                    itemUe.Turmas.AddRange(listaTurmas.Where(x => x.CodigoUe == itemUe.CodigoUe).OrderBy(x => x.CodigoTurma));
-                }
-            }
-        }
+            cabecalhoDto.NomeTurma = await ObterNomeTurma(filtroRelatorio);
 
-        private async Task MapearCabecalho(FrequenciaMensalDto dto, FiltroFrequenciaGlobalDto filtroRelatorio)
-        {
-            dto.Cabecalho.NomeTurma = await ObterNomeTurma(filtroRelatorio);
-
-            await ObterNomeDreUe(filtroRelatorio.CodigoDre, filtroRelatorio.CodigoUe, dto);
-            dto.Cabecalho.AnoLetivo = filtroRelatorio.AnoLetivo;
-            dto.Cabecalho.NomeModalidade = filtroRelatorio.Modalidade.Name();
-            dto.Cabecalho.RfUsuarioSolicitante = filtroRelatorio.UsuarioRf;
-            dto.Cabecalho.UsuarioSolicitante = filtroRelatorio.UsuarioNome;
+            await ObterNomeDreUe(filtroRelatorio.CodigoDre, filtroRelatorio.CodigoUe,cabecalhoDto);
+            cabecalhoDto.AnoLetivo = filtroRelatorio.AnoLetivo;
+            cabecalhoDto.NomeModalidade = filtroRelatorio.Modalidade.Name();
+            cabecalhoDto.RfUsuarioSolicitante = filtroRelatorio.UsuarioRf;
+            cabecalhoDto.UsuarioSolicitante = filtroRelatorio.UsuarioNome;
 
             if (filtroRelatorio.MesesReferencias.Count() == 1 && !filtroRelatorio.MesesReferencias.Contains("-99"))
-                dto.Cabecalho.MesReferencia = ObterNomeMesReferencia(int.Parse(filtroRelatorio.MesesReferencias.FirstOrDefault()));
+                cabecalhoDto.MesReferencia = ObterNomeMesReferencia(int.Parse(filtroRelatorio.MesesReferencias.FirstOrDefault()));
             else if (filtroRelatorio.MesesReferencias.Count() == 1 && filtroRelatorio.MesesReferencias.Contains("-99"))
-                dto.Cabecalho.MesReferencia = "Todos";
+                cabecalhoDto.MesReferencia = "Todos";
         }
-        private void MapearFrequenciaMensalDre(List<IGrouping<string, FrequenciaGlobalDto>> listadres, FrequenciaMensalDto dto)
-        {
-            foreach (var itemDre in listadres.OrderBy(x => x.Key))
-            {
-                var freqDre = new FrequenciaMensalDreDto
-                {
-                    CodigoDre = itemDre.Key,
-                    NomeDre = itemDre.FirstOrDefault().SiglaDre,
-                };
-
-                dto.Dres.Add(freqDre);
-            }
-        }
-
-        private void MapearFrequenciaMensalUe(List<IGrouping<string, FrequenciaGlobalDto>> listaDeFrequenciaUes, List<FrequenciaMensalDreDto> frequenciaMensalDre)
-        {
-            var listaUes = new List<FrequenciaMensalUeDto>();
-            foreach (var item in listaDeFrequenciaUes)
-            {
-                var freqUe = new FrequenciaMensalUeDto 
-                {
-                    CodigoUe = item.Key,
-                    CodigoDre = item.FirstOrDefault().DreCodigo,
-                    NomeUe = item.FirstOrDefault().UeNome
-                };
-                listaUes.Add(freqUe);
-            }
-            foreach (var item in frequenciaMensalDre)
-                item.Ues.AddRange(listaUes.Where(x => x.CodigoDre == item.CodigoDre).OrderBy(x => x.NomeUe));
-        }
-
         private async Task<string> ObterNomeTurma(FiltroFrequenciaGlobalDto filtroRelatorio)
         {
             var nomeTurma = String.Empty;
@@ -197,24 +108,24 @@ namespace SME.SR.Application
 
             return nomeTurma;
         }
-        private async Task ObterNomeDreUe(string dreCodigo, string ueCodigo, FrequenciaMensalDto dto)
+        private async Task ObterNomeDreUe(string dreCodigo, string ueCodigo, FrequenciaMensalCabecalhoDto cabecalhoDto)
         {
             if (!dreCodigo.Contains("-99") && !ueCodigo.Contains("-99"))
             {
                 var dadosDreUe = await mediator.Send(new ObterDreUePorDreCodigoQuery(dreCodigo, ueCodigo));
-                dto.Cabecalho.NomeUe = dadosDreUe.UeNome;
-                dto.Cabecalho.NomeDre = dadosDreUe.DreNome;
+                cabecalhoDto.NomeUe = dadosDreUe.UeNome;
+                cabecalhoDto.NomeDre = dadosDreUe.DreNome;
             }
             else if (!dreCodigo.Contains("-99") && ueCodigo.Contains("-99"))
             {
                 var dadosDre = await mediator.Send(new ObterDrePorCodigoQuery(dreCodigo));
-                dto.Cabecalho.NomeDre = dadosDre.Nome;
-                dto.Cabecalho.NomeUe = "Todas";
+                cabecalhoDto.NomeDre = dadosDre.Nome;
+                cabecalhoDto.NomeUe = "Todas";
             }
             else if (dreCodigo.Contains("-99") && ueCodigo.Contains("-99"))
             {
-                dto.Cabecalho.NomeUe = "Todas";
-                dto.Cabecalho.NomeDre = "Todas";
+                cabecalhoDto.NomeUe = "Todas";
+                cabecalhoDto.NomeDre = "Todas";
             }
         }
         private async Task ExecuteExcel(List<FrequenciaGlobalDto> listaDeFrequencia, Guid codigoCorrelacao)
--- src/SME.SR.Infra/Dtos/Relatorios/FrequenciaGlobal/FrequenciaMensalDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/FrequenciaGlobal/FrequenciaMensalDto.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections.Generic;
-
-namespace SME.SR.Infra
+﻿namespace SME.SR.Infra
 {
     public class FrequenciaMensalDto
     {
-        public FrequenciaMensalDto()
-        {
-            Cabecalho = new FrequenciaMensalCabecalhoDto();
-            Dres = new List<FrequenciaMensalDreDto>();
-        }
-        public FrequenciaMensalCabecalhoDto Cabecalho { get; set; }
-        public List<FrequenciaMensalDreDto> Dres { get; set; }
+        public string CodigoDre { get; set; }
+        public string NomeDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string NomeUe { get; set; }
+        public string NomeTurma { get; set; }
+        public string CodigoTurma { get; set; }
+        public string NomeMes { get; set; }
+        public int ValorMes { get; set; }
+        public string CodigoAluno { get; set; }
+        public string NumeroAluno { get; set; }
+        public string NomeAluno { get; set; }
+        public decimal? ProcentagemFrequencia { get; set; }
     }
 }
