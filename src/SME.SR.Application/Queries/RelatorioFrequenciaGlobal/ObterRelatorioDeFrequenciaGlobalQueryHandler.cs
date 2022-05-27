08d1704192a6225dfb89127e07725f2800159853
Fernando Groeler <fernando.groeler@gmail.com>
2022-05-27 11:08:43 -0400

    Implementado para gerar o relatório de frequência mensal sem registros caso não exista frequência registrada.

--- src/SME.SR.Application/Queries/RelatorioFrequenciaGlobal/ObterRelatorioDeFrequenciaGlobalQueryHandler.cs
+++ src/SME.SR.Application/Queries/RelatorioFrequenciaGlobal/ObterRelatorioDeFrequenciaGlobalQueryHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using SME.SR.Data;
 using SME.SR.Data.Interfaces;
 using SME.SR.Infra;
 using SME.SR.Infra.Utilitarios;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,7 +30,97 @@ namespace SME.SR.Application
                 request.Filtro.CodigoDre, request.Filtro.CodigoUe, request.Filtro.Modalidade, request.Filtro.Semestre, request.Filtro.CodigosTurmas.Select(c => c).ToArray(),
                 request.Filtro.MesesReferencias.Select(c => Convert.ToInt32(c)).ToArray(), request.Filtro.ApenasAlunosPercentualAbaixoDe);
 
+            if (retornoQuery == null || (retornoQuery.Count() == 0))
+                return await MapearRetornoVazio(request.Filtro);
+
             return await MapearRetornoQuery(request.Filtro, retornoQuery);
+        }
+
+        private async Task<List<FrequenciaGlobalDto>> MapearRetornoVazio(FiltroFrequenciaGlobalDto filtro)
+        {
+            var retornoMapeado = new List<FrequenciaGlobalDto>();
+
+            List<Dre> dres;
+            List<Turma> turmas;
+
+            if (string.IsNullOrEmpty(filtro.CodigoDre) || (filtro.CodigoDre == "-99"))
+                dres = (await _mediator.Send(new ObterTodasDresQuery())).ToList();
+            else
+                dres = new List<Dre> { await _mediator.Send(new ObterDrePorCodigoQuery(filtro.CodigoDre)) };
+
+            var ues = (await _mediator.Send(new ObterPorDresIdQuery(dres.Select(c => c.Id).ToArray()))).ToList();
+
+            if (!string.IsNullOrEmpty(filtro.CodigoUe) && (filtro.CodigoUe != "-99"))
+                ues = ues.Where(c => c.Codigo == filtro.CodigoUe).ToList();
+
+            var meses = filtro.MesesReferencias;
+
+            if ((meses.Count == 0) || meses.Contains("-99"))
+                meses = new List<string> { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+
+            foreach (var dre in dres)
+            {
+                foreach (var ue in ues)
+                {
+                    if ((filtro.CodigosTurmas.Count() == 0) || filtro.CodigosTurmas.Contains("-99"))
+                        turmas = (await _mediator.Send(new ObterTurmasPorUeEAnoLetivoQuery(ue.Codigo, filtro.AnoLetivo))).ToList();
+                    else
+                    {
+                        turmas = (await _mediator.Send(new ObterTurmasPorCodigoQuery(filtro.CodigosTurmas.Select(c => c).ToArray()))).ToList();
+
+                        //-> Foi necessário fazer isso, pois, a query acima retorna no campo código.
+                        turmas.ForEach(c => c.turma_id = c.Codigo);
+                    }
+
+                    var alunos = await _mediator.Send(new ObterAlunosPorTurmasQuery(turmas.Select(c => long.Parse(c.turma_id))));
+
+                    var dadosAlunosEscolas = await _mediator.Send(new ObterDadosAlunosEscolaQuery(ue.Codigo, filtro.AnoLetivo,
+                        alunos.Select(c => c.CodigoAluno.ToString()).ToArray()));
+
+                    var agrupamento = dadosAlunosEscolas.OrderBy(x => x.CodigoAluno)
+                        .GroupBy(x => new { x.CodigoAluno, x.NomeAluno, x.CodigoTurma });
+
+                    foreach (var turma in turmas)
+                    {
+                        foreach (var mes in meses)
+                        {
+                            var dadosSituacaoAluno = DeveImprimirNoRelatorio(agrupamento, int.Parse(mes));
+
+                            if (dadosSituacaoAluno.ImprimirRelatorio)
+                            {
+                                foreach (var item in agrupamento.SelectMany(c => c))
+                                {
+                                    var aluno = dadosAlunosEscolas.Select(c => new { c.CodigoAluno, c.NomeAluno, c.NomeSocialAluno, c.NumeroAlunoChamada, c.CodigoTurma })
+                                        .FirstOrDefault(c => c.CodigoAluno == item.CodigoAluno);
+
+                                    retornoMapeado.Add(new FrequenciaGlobalDto()
+                                    {
+                                        SiglaDre = dre.Abreviacao,
+                                        DreCodigo = dre.Codigo,
+                                        UeNome = string.Concat(ue.TipoEscola.GetAttribute<DisplayAttribute>().ShortName, " - ", ue.Nome),
+                                        UeCodigo = ue.Codigo,
+                                        Mes = int.Parse(mes),
+                                        TurmaCodigo = turma.Codigo,
+                                        Turma = string.Concat(turma.ModalidadeCodigo.GetAttribute<DisplayAttribute>().ShortName, " - ", turma.Nome),
+                                        CodigoEOL = aluno.CodigoAluno.ToString(),
+                                        Estudante = string.IsNullOrEmpty(aluno.NomeSocialAluno) ? aluno.NomeAluno : aluno.NomeSocialAluno,
+                                        NumeroChamadda = aluno.NumeroAlunoChamada,
+                                        PercentualFrequencia = null
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            var retornoOrdenado = retornoMapeado.OrderBy(c => c.SiglaDre)
+                .ThenBy(c => c.UeNome)
+                .ThenBy(c => c.Mes)
+                .ThenBy(c => c.Turma)
+                .ThenBy(c => c.Estudante);
+
+            return retornoOrdenado.ToList();
         }
 
         private async Task<List<FrequenciaGlobalDto>> MapearRetornoQuery(FiltroFrequenciaGlobalDto filtro,
