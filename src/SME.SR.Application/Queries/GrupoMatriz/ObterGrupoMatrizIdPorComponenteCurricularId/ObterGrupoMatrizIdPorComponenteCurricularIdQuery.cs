e5c5890d8afa1d370162394ab2f84b2f43143ff0
Helio Barros <helio.barros@amcom.com.br>
2022-07-29 12:54:24 -0300

    AB#70864 - ajuste para obter o id do grupo matriz do componente curricular no relatório de ata do conselho de classe (jasper)

---
+++ src/SME.SR.Application/Queries/GrupoMatriz/ObterGrupoMatrizIdPorComponenteCurricularId/ObterGrupoMatrizIdPorComponenteCurricularIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Application
+{
+    public class ObterGrupoMatrizIdPorComponenteCurricularIdQuery : IRequest<long>
+    {
+        public long ComponenteCurricularId { get; set; }
+
+        public ObterGrupoMatrizIdPorComponenteCurricularIdQuery(long componenteCurricularId)
+        {
+            ComponenteCurricularId = componenteCurricularId;
+        }
+    }
+}
