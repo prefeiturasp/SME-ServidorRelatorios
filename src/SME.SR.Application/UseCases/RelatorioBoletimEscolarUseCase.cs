48ce91ff7ded65b66458b3c2cf39852ac7635d03
Robson <robson.silva@amcom.com.br>
2022-08-04 15:54:12 -0300

    Ajuste para tentar capturar o problema da não publicação da mensagem na fila de prontos do boletim.

--- src/SME.SR.Application/UseCases/RelatorioBoletimEscolarUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioBoletimEscolarUseCase.cs
@@ -20,12 +20,19 @@ namespace SME.SR.Workers.SGP
 
         public async Task Executar(FiltroRelatorioDto request)
         {
-            request.RotaErro = RotasRabbitSGP.RotaRelatoriosComErroBoletim;
-            var relatorioQuery = request.ObterObjetoFiltro<ObterRelatorioBoletimEscolarQuery>();
-            var relatorio = await mediator.Send(relatorioQuery);
+            try
+            {
+                request.RotaErro = RotasRabbitSGP.RotaRelatoriosComErroBoletim;
+                var relatorioQuery = request.ObterObjetoFiltro<ObterRelatorioBoletimEscolarQuery>();
+                var relatorio = await mediator.Send(relatorioQuery);
 
-            await mediator.Send(new GerarRelatorioHtmlParaPdfCommand("RelatorioBoletimEscolarSimples",
-                relatorio, request.CodigoCorrelacao));
+                await mediator.Send(new GerarRelatorioHtmlParaPdfCommand("RelatorioBoletimEscolarSimples",
+                    relatorio, request.CodigoCorrelacao));
+            }
+            catch (Exception ex)
+            {
+                await mediator.Send(new SalvarLogViaRabbitCommand($"Boletim - {ex.ToString()}", LogNivel.Critico, "Relatórios - Boletim"));
+            }            
         }
     }
 }
