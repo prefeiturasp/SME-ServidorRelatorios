ce2353a1d54090b7a485745b9cefc49b32b36940
jailsvs <jailsvs@gmail.com>
2022-10-20 11:01:04 -0300

    75091 - Ajuste apresentação de recomendações texto e pré definidas/selecionadas no relatório ata conselho

--- src/SME.SR.Data/Interfaces/Sgp/IConselhoClasseAlunoRepository.cs
+++ src/SME.SR.Data/Interfaces/Sgp/IConselhoClasseAlunoRepository.cs
@@ -11,7 +11,7 @@ namespace SME.SR.Data.Interfaces
 
         Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterParecerConclusivoPorTurma(string turmaCodigo);
 
-        Task<RecomendacaoConselhoClasseAluno> ObterRecomendacoesPorFechamento(long fechamentoTurmaId, string codigoAluno);
+        Task<RecomendacoesConselhoClasse> ObterRecomendacoesPorFechamento(long fechamentoTurmaId, string codigoAluno);
 
         Task<IEnumerable<RecomendacaoConselhoClasseAluno>> ObterRecomendacoesPorAlunosTurmas(string[] codigosAluno, string[] codigosTurma, int anoLetivo, Modalidade? modalidade, int semestre);
 
@@ -19,6 +19,5 @@ namespace SME.SR.Data.Interfaces
         Task<IEnumerable<AnotacoesPedagogicasAlunoIdsQueryDto>> ObterAnotacoesPedagogicasPorConselhoClasseAlunoIdsAsync(long[] conselhoClasseAlunoIds);
 
         Task<IEnumerable<RecomendacoesAlunoFamiliaDto>> ObterRecomendacoesAlunoFamiliaPorAlunoETurma(string codigoAluno, string codigoTurma, int id);
-        Task<IEnumerable<RecomendacoesAlunoFamiliaDto>> ObterRecomendacoesAlunoFamiliaPorAlunoEFechamentoTurma(long fechamentoTurmaId, string codigoAluno);
     }
 }
---
+++ src/SME.SR.Data/Models/RecomendacoesConselhoClasse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SME.SR.Data
+{
+    public class RecomendacoesConselhoClasse
+    {
+        public RecomendacoesConselhoClasse()
+        {
+            RecomendacoesPreDefinidasAluno = new List<ConselhoClasseRecomendacao>();
+            RecomendacoesPreDefinidasFamilia = new List<ConselhoClasseRecomendacao>();
+        }
+
+        public long ConselhoClasseAlunoId { get; set; }
+        public long ConselhoClasseId { get; set; }
+        public string RecomendacoesAluno { get; set; }
+        public string RecomendacoesFamilia { get; set; }
+        public string AnotacoesPedagogicas { get; set; }
+       public List<ConselhoClasseRecomendacao> RecomendacoesPreDefinidasAluno { get; set; }
+       public List<ConselhoClasseRecomendacao> RecomendacoesPreDefinidasFamilia { get; set; }
+    }
+}
