763f911baccc0599486e0a6c665206c1a64537c2
Felipe Abreu <felipe.abreu@amcom.com.br>
2022-11-23 18:08:25 -0300

    Correção relatório de alteração de notas - Fechamento e por Conselho.

--- src/SME.SR.Application/Queries/HistoricoAlteracaoNotas/ObterHistoricoNotasConselhoClassePorTurmaId/ObterHistoricoNotasConselhoClassePorTurmaIdQuery.cs
+++ src/SME.SR.Application/Queries/HistoricoAlteracaoNotas/ObterHistoricoNotasConselhoClassePorTurmaId/ObterHistoricoNotasConselhoClassePorTurmaIdQuery.cs
@@ -6,13 +6,17 @@ namespace SME.SR.Application
 {
     public class ObterHistoricoNotasConselhoClassePorTurmaIdQuery : IRequest<IEnumerable<HistoricoAlteracaoNotasDto>>
     {
-        public ObterHistoricoNotasConselhoClassePorTurmaIdQuery(long turmaId, long tipoCalendarioId)
+        public ObterHistoricoNotasConselhoClassePorTurmaIdQuery(long turmaId, long tipoCalendarioId, int[] bimestres , long[] componentes)
         {
             TurmaId = turmaId;
             this.TipoCalendarioId = tipoCalendarioId;
+            Bimestres = bimestres;
+            Componentes = componentes;
         }
 
         public long TurmaId { get; set; }
         public long TipoCalendarioId { get; set; }
+        public int[] Bimestres { get; set; }
+        public long[] Componentes { get; set; }
     }
 }
