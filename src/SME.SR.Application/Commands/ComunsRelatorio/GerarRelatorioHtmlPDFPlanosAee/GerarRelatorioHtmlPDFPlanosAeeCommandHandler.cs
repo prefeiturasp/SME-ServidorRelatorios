d0b9c58c1cbdb05f411782c0460d98ef4162c2e9
Fernando Groeler <fernando.groeler@gmail.com>
2022-12-14 09:31:06 -0400

    Ajustado critério 8 da estória.

--- src/SME.SR.Application/Commands/ComunsRelatorio/GerarRelatorioHtmlPDFPlanosAee/GerarRelatorioHtmlPDFPlanosAeeCommandHandler.cs
+++ src/SME.SR.Application/Commands/ComunsRelatorio/GerarRelatorioHtmlPDFPlanosAee/GerarRelatorioHtmlPDFPlanosAeeCommandHandler.cs
@@ -45,7 +45,7 @@ namespace SME.SR.Application
             var pdfGenerator = new PdfGenerator(converter);
 
             var caminhoBase = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "relatorios");
-            pdfGenerator.ConvertToPdfPaginacaoSolo(paginasSolo, caminhoBase, request.CodigoCorrelacao.ToString(), "Relatório do Planos Aee");
+            pdfGenerator.ConvertToPdfPaginacaoSolo(paginasSolo, caminhoBase, request.CodigoCorrelacao.ToString(), "Relatório dos Planos Aee");
 
             await servicoFila.PublicaFila(new PublicaFilaDto(new MensagemRelatorioProntoDto(), RotasRabbitSGP.RotaRelatoriosProntosSgp, ExchangeRabbit.Sgp, request.CodigoCorrelacao));
         }
