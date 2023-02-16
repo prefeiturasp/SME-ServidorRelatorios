929dee32bdd9f320a4d9b0bfd5e18c203287a6df
jailsvs <jailsvs@gmail.com>
2023-02-16 16:27:38 -0300

    86141 - Busca de professor titular de componente por atribuição externa

--- src/SME.SR.Application/UseCases/RelatorioAcompanhamentoAprendizagemUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioAcompanhamentoAprendizagemUseCase.cs
@@ -27,7 +27,12 @@ namespace SME.SR.Application
                 throw new NegocioException("Turma não encontrada");
 
             var turmaCodigo = new string[1] { turma.Codigo };
-            var professores = await mediator.Send(new ObterProfessorTitularComponenteCurricularPorTurmaQuery(turmaCodigo));
+            var professores = (await mediator.Send(new ObterProfessorTitularComponenteCurricularPorTurmaQuery(turmaCodigo))).ToList();
+
+            var uesIndiretas = new TipoEscola[] { TipoEscola.CRPCONV, TipoEscola.CEIINDIR };
+            if (uesIndiretas.Contains(turma.Ue.TipoEscola))
+                professores.AddRange(await mediator.Send(new ObterProfessorTitularExternoComponenteCurricularPorTurmaQuery(turmaCodigo)));
+            
 
             var alunosEol = await mediator.Send(new ObterAlunosPorTurmaAcompanhamentoApredizagemQuery(turma.Codigo, parametros.AlunoCodigo, turma.AnoLetivo));
             if (alunosEol == null || !alunosEol.Any())
--- src/SME.SR.Data/Repositories/Eol/ProfessorRepository.cs
+++ src/SME.SR.Data/Repositories/Eol/ProfessorRepository.cs
@@ -140,5 +140,65 @@ namespace SME.SR.Data
                 return result.ToList();
             }
         }
+
+        public async Task<IEnumerable<ProfessorTitularComponenteCurricularDto>> BuscarProfessorTitularExternoComponenteCurricularPorTurma(string[] codigosTurma)
+        {
+            StringBuilder query = new StringBuilder();
+
+            query.AppendLine(@"
+				select coalesce(cc_pro.cd_componente_curricular, cc_ser.cd_componente_curricular) as ComponenteCurricularId,
+					   coalesce(cc_pro.dc_componente_curricular, cc_ser.dc_componente_curricular) as ComponenteCurricular,
+					   coalesce(p.cd_cpf_pessoa, '')                                   as ProfessorRF,
+					   coalesce(p.nm_pessoa, N'Não há professor titular.')                     as NomeProfessor,
+                       tur.cd_turma_escola TurmaCodigo
+                from turma_escola tur
+                         -- Turma regular
+                         left join serie_turma_grade ste on tur.cd_turma_escola = ste.cd_turma_escola and ste.dt_fim is null
+                         left join escola_grade eg_ser
+                                   on ste.cd_escola = eg_ser.cd_escola and ste.cd_escola_grade = eg_ser.cd_escola_grade
+                         left join grade g_ser on eg_ser.cd_grade = g_ser.cd_grade
+                         left join grade_componente_curricular gcc_ser on g_ser.cd_grade = gcc_ser.cd_grade
+                         left join componente_curricular cc_ser on gcc_ser.cd_componente_curricular = cc_ser.cd_componente_curricular
+
+                    -- Turma Programa
+                         left join turma_escola_grade_programa tegp
+                                   on tegp.cd_turma_escola = tur.cd_turma_escola and tegp.dt_fim is null
+                         left join escola_grade eg_pro on eg_pro.cd_escola_grade = tegp.cd_escola_grade
+                         left join grade g_pro on eg_pro.cd_grade = g_pro.cd_grade
+                         left join grade_componente_curricular gcc_pro on g_pro.cd_grade = gcc_pro.cd_grade
+                         left join componente_curricular cc_pro on gcc_pro.cd_componente_curricular = cc_pro.cd_componente_curricular
+
+                    -- Atribuicao turma regular
+                         left join atribuicao_externo atb_ext
+                                   on gcc_ser.cd_grade = atb_ext.cd_grade and
+                                      gcc_ser.cd_componente_curricular = atb_ext.cd_componente_curricular
+                                       and atb_ext.cd_serie_grade = ste.cd_serie_grade and atb_ext.dt_cancelamento is null
+                                       and (atb_ext.dt_disponibilizacao is null)
+                                       and atb_ext.an_atribuicao = year(getdate())
+
+                    -- Atribuicao turma programa
+                         left join atribuicao_externo atb_ext_prog
+                                   on gcc_pro.cd_grade = atb_ext_prog.cd_grade and
+                                      gcc_pro.cd_componente_curricular = atb_ext_prog.cd_componente_curricular and
+                                      atb_ext_prog.cd_turma_escola_grade_programa = tegp.cd_turma_escola_grade_programa and
+                                      atb_ext_prog.dt_cancelamento is null
+                                       and atb_ext_prog.dt_disponibilizacao is null
+                                       and atb_ext_prog.an_atribuicao = year(getdate())
+
+                    inner join contrato_externo ce with (NOLOCK) on ce.cd_motivo_desligamento_externo is null and (ce.cd_contrato_externo = atb_ext.cd_contrato_externo or ce.cd_contrato_externo = atb_ext_prog.cd_contrato_externo)
+                    -- Servidor                   	
+                    inner join pessoa p with (NOLOCK) on p.cd_pessoa = ce.cd_pessoa 
+                    WHERE tur.cd_turma_escola in @codigosTurma");
+
+            var parametros = new { codigosTurma };
+
+            using (var conn = new SqlConnection(variaveisAmbiente.ConnectionStringEol))
+            {
+                var result = await conn.QueryAsync<ProfessorTitularComponenteCurricularDto>(
+                                              query.ToString(),
+                                              parametros);
+                return result.ToList();
+            }
+        }
     }
 }
