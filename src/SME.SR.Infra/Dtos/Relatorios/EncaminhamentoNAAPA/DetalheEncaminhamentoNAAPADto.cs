c14ba2ee0e5940b2dbca3305ef6b765034eb0085
jailsvs <jailsvs@gmail.com>
2023-03-06 11:51:43 -0300

    87410 - Adição de arquivos ignorados git

---
+++ src/SME.SR.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/DetalheEncaminhamentoNAAPADto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class DetalheEncaminhamentoNAAPADto
+    {
+        public string Aluno { get; set; }
+        public string Turma { get; set; }
+        public DateTime DataEntradaQueixa { get; set; }
+        public string PortaEntrada { get; set; }
+        public string FluxosAlerta { get; set; }
+        public DateTime DataUltimoAtendimento { get; set; }
+
+    }
+}
