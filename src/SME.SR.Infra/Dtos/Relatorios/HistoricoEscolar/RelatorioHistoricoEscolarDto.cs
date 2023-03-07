61d1e66f136354893dd0596ff35210e523c9468f
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-03-07 10:36:42 -0300

    adicionando classe

---
+++ src/SME.SR.Infra/Dtos/Relatorios/HistoricoEscolar/RelatorioHistoricoEscolarDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class RelatorioHistoricoEscolarDto
+    {
+        public int TotalPagina { get; set; }
+
+        public IEnumerable<RelatorioPaginadoHistoricoEscolarDto> RelatorioPaginadados { get; set; }
+    }
+}
