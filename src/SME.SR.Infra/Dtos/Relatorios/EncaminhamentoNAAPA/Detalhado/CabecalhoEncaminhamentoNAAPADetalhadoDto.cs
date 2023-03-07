6195e726b729b1da322bde5fc12ff9559be72c34
Juliano Martins <juliano_campos_92@hotmail.com>
2023-03-07 13:57:37 -0400

    dto com dados do relatório

---
+++ src/SME.SR.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/Detalhado/CabecalhoEncaminhamentoNAAPADetalhadoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class CabecalhoEncaminhamentoNAAPADetalhadoDto
+    {
+        public int AnoLetivo { get; set; }
+        public string DreNome { get; set; }
+        public string UeNome { get; set; }
+        public string TurmaNome { get; set; }
+        public string Aluno { get; set; }
+        public string SituacaoEncaminhamento { get; set; }
+        public string UsuarioNome { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/Detalhado/QuestaoEncaminhamentoNAAPADetalhadoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SME.SR.Infra
+{
+    public class QuestaoEncaminhamentoNAAPADetalhadoDto
+    {
+        public string Questao { get; set; }
+        public long QuestaoId { get; set; }
+        public TipoQuestao TipoQuestao { get; set; }
+        public int Ordem { get; set; }
+        public string OrdemMascara { get; set; }
+        public string Resposta { get; set; }
+    }
+}
