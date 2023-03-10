d36a86b0a3b574a37d2ed794d62bca18ceb6aaa8
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-03-10 16:52:41 -0300

    Layout do relatório e Quebra

---
+++ src/SME.SR.Infra/RelatorioPaginado/EncaminhamentoNaapa/EncaminhamentoNaapaDetalhadoPagina.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra
+{
+    public class EncaminhamentoNaapaDetalhadoPagina
+    {
+        public CabecalhoEncaminhamentoNAAPADetalhadoDto Cabecalho { get; set; }
+        public List<SecaoRelatorioEncaminhamentoNaapa> Linhas { get; set; }
+        public int Pagina { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/RelatorioPaginado/EncaminhamentoNaapa/SecaoRespostaTextoEncaminhamentoNaapa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SME.SR.Infra
+{
+    public class SecaoRespostaTextoEncaminhamentoNaapa : SecaoRelatorioEncaminhamentoNaapa
+    {
+        private const int QDADE_CHARS_POR_LINHA = 100;
+
+        public SecaoRespostaTextoEncaminhamentoNaapa(QuestaoEncaminhamentoNAAPADetalhadoDto questao, bool semTitulo = false)
+        {
+            Titulo = semTitulo ? string.Empty : questao.Questao.ToUpper();
+            Resposta = questao.Resposta;
+        }
+
+        public string Titulo { get; set; }
+        public string Resposta {  get; set; }
+
+        public override int ObterLinhasDeQuebra()
+        {
+            var linha = string.IsNullOrEmpty(Titulo) ? 1 : 2;
+
+            if (!string.IsNullOrEmpty(Resposta) && Resposta.Length > QDADE_CHARS_POR_LINHA)
+                return 1 + (int)Math.Round((double)(Resposta.Length / QDADE_CHARS_POR_LINHA));
+
+            return linha;
+        }
+    }
+}
