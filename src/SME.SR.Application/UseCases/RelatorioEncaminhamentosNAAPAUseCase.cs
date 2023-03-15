4599d5161f0d058851d40242d7a0448a86d7398a
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-03-15 17:45:25 -0300

    88906 - alteração do command

--- src/SME.SR.Application/UseCases/RelatorioEncaminhamentosNAAPAUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioEncaminhamentosNAAPAUseCase.cs
@@ -60,7 +60,7 @@ namespace SME.SR.Application
                 UsuarioNome = $"{filtroRelatorio.UsuarioNome} ({filtroRelatorio.UsuarioRf})",
             };
             relatorio.EncaminhamentosDreUe = encaminhamentosAgrupados;
-            await mediator.Send(new GerarRelatorioHtmlParaPdfCommand("RelatorioEncaminhamentosNAAPA", relatorio, request.CodigoCorrelacao, tipoDePaginacao: EnumTipoDePaginacao.PaginaComTotalPaginas));
+            await mediator.Send(new GerarRelatorioHtmlPDFEncaminhamentoNaapaCommad(relatorio, request.CodigoCorrelacao));
         }
     }
 }
