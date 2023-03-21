4146d7b5b80f37941a4da791ebd6be8d45a782fd
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-03-21 09:06:56 -0300

    88959 - Criar estrutura excel SR - Leitura

---
+++ src/SME.SR.Application/Queries/Sondagem/ObterRelatorioAnaliticoSondagemExcel/ObterRelatorioAnaliticoSondagemExcelQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using SME.SR.Infra.Excel.Sondagem;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SR.Application
+{
+    public class ObterRelatorioAnaliticoSondagemExcelQueryHandler : IRequestHandler<ObterRelatorioAnaliticoSondagemExcelQuery, IEnumerable<RelatorioSondagemAnaliticoExcelDto>>
+    {
+        public async Task<IEnumerable<RelatorioSondagemAnaliticoExcelDto>> Handle(ObterRelatorioAnaliticoSondagemExcelQuery request, CancellationToken cancellationToken)
+        {
+            var dicionario = ObterDicionarioGeradorDataTable(request.RelatorioAnalitico);
+
+            if (dicionario.ContainsKey(request.TipoSondagem))
+                return await Task.FromResult(dicionario[request.TipoSondagem].ObterTabelaExcel());
+
+            return await Task.FromResult(Enumerable.Empty<RelatorioSondagemAnaliticoExcelDto>());
+        }
+
+        private Dictionary<TipoSondagem, GeradorDeTabelaExcelAnaliticoSondagem> ObterDicionarioGeradorDataTable(IEnumerable<RelatorioSondagemAnaliticoPorDreDto> relatorios)
+        {
+            return new Dictionary<TipoSondagem, GeradorDeTabelaExcelAnaliticoSondagem>()
+            {
+                { TipoSondagem.LP_Leitura, new GeradorDeTabelaExcelAnaliticoSondagemLeitura(relatorios) },
+                { TipoSondagem.LP_LeituraVozAlta, new GeradorDeTabelaExcelAnaliticoSondagemLeituraDeVozAlta(relatorios) }
+            };
+        }
+    }
+}
--- src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioAnalitico/RelatorioSondagemAnaliticoCapacidadeDeLeituraDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioAnalitico/RelatorioSondagemAnaliticoCapacidadeDeLeituraDto.cs
@@ -5,5 +5,6 @@ namespace SME.SR.Infra
     public class RelatorioSondagemAnaliticoCapacidadeDeLeituraDto : RelatorioSondagemAnaliticoPorDreDto
     {
         public List<RespostaSondagemAnaliticoCapacidadeDeLeituraDto> Respostas { get; set; }
+        protected override TipoSondagem TipoDaSondagem => TipoSondagem.LP_CapacidadeLeitura;
     }
 }
