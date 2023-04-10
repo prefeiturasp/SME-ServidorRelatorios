2b2ba222b387c8b379737df8be3485de8fdb7245
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-04-10 10:47:13 -0300

    90003 - Adicionando Dto

---
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioAnalitico/RespostaMatematicaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SME.SR.Infra
+{
+    public class RespostaMatematica
+    {
+        public int Acertou {  get; set; }   
+        public int Resolveu { get; set; }
+        public int NaoResolveu { get; set; }
+        public int SemPreenchimento { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/RelatorioAnalitico/RespostaSondagemAnaliticoCampoAditivoMultiplicativoDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SR.Infra.Dtos.Relatorios.Sondagem.RelatorioAnalitico
+{
+    internal class RespostaSondagemAnaliticoCampoAditivoDto
+    {
+    }
+}
