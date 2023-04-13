e0fb32c99f6bef7da12113fbb64d07f70a5ed75d
Thiago Fernando <thiago.fernando@msn.com>
2023-04-13 09:59:10 -0300

    Adiciondo SQL IAD antes 2022

---
+++ src/SME.SR.Infra/Dtos/Relatorios/Sondagem/PerguntaRelatorioMatematicaNumerosDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SME.SR.Infra
+{
+    public class PerguntaRelatorioMatematicaNumerosDto
+    {
+        public string PerguntaId { get; set; }
+        public string PerguntaDescricao { get; set; }
+        public string RespostaId { get; set; }
+        public string RespostaDescricao { get; set; }
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string CodigoTurma { get; set; }
+        public int AnoTurma { get; set; }
+        public int QtdRespostas { get; set; }
+        public int OrdemReposta { get; set; }
+        public int OrdemPergunta { get; set; }
+    }
+}
