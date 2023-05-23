fedd7644a9b4c625ce49fe85f9fdf0a617fe0d96
Felipe Abreu <felipe.abreu@amcom.com.br>
2023-05-23 16:47:06 -0300

    Corrigindo falha ao gerar relatório controle de grade.

--- src/SME.SR.Application/Queries/Aula/VerificarAulasNormaisCriadasProfessorRegencia/VerificarAulasNormaisCriadasProfessorRegenciaQuery.cs
+++ src/SME.SR.Application/Queries/Aula/VerificarAulasNormaisCriadasProfessorRegencia/VerificarAulasNormaisCriadasProfessorRegenciaQuery.cs
@@ -4,13 +4,15 @@ namespace SME.SR.Application
 {
     public class VerificarAulasNormaisCriadasProfessorRegenciaQuery : IRequest<bool>
     {
-        public VerificarAulasNormaisCriadasProfessorRegenciaQuery(string componenteCurricularId, int bimestre, long tipoCalendarioId)
+        public VerificarAulasNormaisCriadasProfessorRegenciaQuery(long turmaId, string componenteCurricularId, int bimestre, long tipoCalendarioId)
         {           
+            Turmaid = turmaId;
             ComponenteCurricularId = componenteCurricularId;
             Bimestre = bimestre;
             TipoCalendarioId = tipoCalendarioId;
         }
 
+        public long Turmaid { get; set; }
         public string ComponenteCurricularId { get; set; }
         public int Bimestre { get; set; }
         public long TipoCalendarioId { get; set; }
