b5c32fa4741acd11d3d4950319bd287254032cf2
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-06-07 14:33:41 -0300

    90270 - Ajustar Relatório Boletim Simples

--- src/SME.SR.Data/Models/NotaConceitoBimestreComponente.cs
+++ src/SME.SR.Data/Models/NotaConceitoBimestreComponente.cs
@@ -57,12 +57,12 @@ namespace SME.SR.Data
 
         private ConceitoDto ObterConceitoPlenamenteSatisfatorio()
         {
-            return Nota.Value > 7 ? ObterConceito(TipoConceito.PlenamenteSatisfatorio) : null;
+            return Nota.Value >= 7 ? ObterConceito(TipoConceito.PlenamenteSatisfatorio) : null;
         }
 
         private ConceitoDto ObterConceitoSatisfatorio()
         {
-            return Nota.Value >= 5 && Nota.Value <= 7 ? ObterConceito(TipoConceito.Satisfatorio) : null;
+            return Nota.Value >= 5 && Nota.Value < 7 ? ObterConceito(TipoConceito.Satisfatorio) : null;
         }
 
         private ConceitoDto ObterConceitoNaoSatisfatorio()
