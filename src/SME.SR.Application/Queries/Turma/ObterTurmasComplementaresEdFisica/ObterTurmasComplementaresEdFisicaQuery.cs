943fe9cd46151aa52127fab308d8d5ce50122358
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-07-13 08:46:13 -0300

    89373 - Ajuste query mediator de busca turmas por alunos em boletins

--- src/SME.SR.Application/Queries/Turma/ObterTurmasComplementaresEdFisica/ObterTurmasComplementaresEdFisicaQuery.cs
+++ src/SME.SR.Application/Queries/Turma/ObterTurmasComplementaresEdFisica/ObterTurmasComplementaresEdFisicaQuery.cs
@@ -6,13 +6,15 @@ namespace SME.SR.Application
 {
     public class ObterTurmasComplementaresEdFisicaQuery : IRequest<IEnumerable<Turma>>
     {
-        public ObterTurmasComplementaresEdFisicaQuery(string[] codigosTurmas, string[] codigosAlunos)
+        public ObterTurmasComplementaresEdFisicaQuery(string[] codigosTurmas, string[] codigosAlunos, int anoLetivo)
         {
             CodigosTurmas = codigosTurmas;
             CodigosAlunos = codigosAlunos;
+            AnoLetivo = anoLetivo;
         }
 
         public string[] CodigosTurmas { get; set; }
         public string[] CodigosAlunos { get; set; }
+        public int AnoLetivo { get; set; }
     }
 }
---
+++ src/SME.SR.Application/Queries/Turma/ObterTurmasPorAlunosComParecer/ObterTurmasPorAlunosComParecerQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SR.Infra;
+using System.Collections.Generic;
+
+namespace SME.SR.Application
+{
+    public class ObterTurmasPorAlunosComParecerQuery : IRequest<IEnumerable<AlunosTurmasCodigosDto>>
+    {
+        public ObterTurmasPorAlunosComParecerQuery(long[] alunosCodigos, long[] pareceresConclusivosIds)
+        {
+            AlunosCodigos = alunosCodigos;
+            PareceresConclusivosIds = pareceresConclusivosIds;
+        }
+
+        public long[] AlunosCodigos { get; set; }
+        public long[] PareceresConclusivosIds { get; set; }
+
+
+    }
+}
