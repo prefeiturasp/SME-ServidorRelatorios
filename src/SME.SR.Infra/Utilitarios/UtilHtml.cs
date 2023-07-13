200d7b9e3bd2a73c5595456a4d9e718d18f0c2a6
DanylloPereira <danyllo.antunes@gmail.com>
2023-07-13 10:11:43 -0300

    98506 - Boletim exibindo caracteres especiais

--- src/SME.SR.Infra/Utilitarios/UtilHtml.cs
+++ src/SME.SR.Infra/Utilitarios/UtilHtml.cs
@@ -1,4 +1,6 @@
-﻿namespace SME.SR.Infra.Utilitarios
+﻿using System.Text.RegularExpressions;
+
+namespace SME.SR.Infra.Utilitarios
 {
     public static class UtilHtml
     {
@@ -7,15 +9,29 @@
             if (!string.IsNullOrEmpty(textoHtml))
             {
                 string semTags = UtilRegex.RemoverTagsHtmlMidia(textoHtml);
+                semTags = RemoverTagsStyle(semTags);
                 semTags = UtilRegex.RemoverTagsHtml(semTags);
                 semTags = UtilRegex.AdicionarEspacos(semTags);
-
+                
                 return semTags;
             }
             else
             {
                 return textoHtml;
             }
+        }
+
+        private static string RemoverTagsStyle(string textoHtml)
+        {
+            var posicao = textoHtml.IndexOf("<style>");
+            if (posicao > 0)
+            {
+                var posicaoFinal = textoHtml.IndexOf("</style>");
+                if (posicaoFinal > 0)
+                    return textoHtml.Substring(0, posicao) + textoHtml.Substring(posicaoFinal + 8);
+            }
+
+            return textoHtml;
         }
     }
 }
