edccce97ae26f0d8a828c610a76c21bc93e41559
Igor Hebling Sallowicz <igor.sallo@hotmail.com>
2023-08-31 12:49:41 -0300

    Removido nullable para o campo

--- src/SME.SR.Application/Queries/RelatorioFechamentoPendencias/ObterRelatorioFechamentoPedencias/ObterRelatorioPedenciasQueryHandler.cs
+++ src/SME.SR.Application/Queries/RelatorioFechamentoPendencias/ObterRelatorioFechamentoPedencias/ObterRelatorioPedenciasQueryHandler.cs
@@ -47,7 +47,7 @@ namespace SME.SR.Application
             var componentesTerritorioSaberTurma = await mediator.Send(new ObterComponentesTerritorioSaberPorTurmaEComponentesIdsQuery(filtros.TurmasCodigo.FirstOrDefault(), componentesCurricularesIds));
             var turmaSelecionada = await mediator.Send(new ObterTurmaPorCodigoQuery(filtros.TurmasCodigo.FirstOrDefault()));
 
-            var ehTerritorioSaber = (bool)componentesCurricularesDescricoes?.FirstOrDefault().TerritorioSaber;
+            var ehTerritorioSaber = componentesCurricularesDescricoes.First().TerritorioSaber;
 
             var retorno = new RelatorioPendenciasDto();
             var retornoLinearParaCabecalho = resultadoQuery.Where(x => x.DreNome?.Length > 0 && x.UeNome?.Length > 0 && x.OutrasPendencias == false).FirstOrDefault();
