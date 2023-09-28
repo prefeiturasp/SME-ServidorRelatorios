32eaa3c9c49e96388b146fee1a682f315c3898b9
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-28 13:27:03 -0300

    105413 - Ajuste código remoção de consulta duplicada

--- src/SME.SR.Application/Queries/ComponenteCurricular/ObterComponentesCurricularesEolPorIds/ObterComponentesCurricularesEolPorIdsQueryHandler.cs
+++ src/SME.SR.Application/Queries/ComponenteCurricular/ObterComponentesCurricularesEolPorIds/ObterComponentesCurricularesEolPorIdsQueryHandler.cs
@@ -30,20 +30,17 @@ namespace SME.SR.Application
 
             var componenteIds = request.ComponentesCurricularesIds.Select(x => x.ToString()).ToArray();
             var componentesTS = lstComponentes.Where(c => c.TerritorioSaber)
-                                .Select(c => c.Codigo.ToString()).ToArray();
+                                .Select(c => c.Codigo);
 
-            IEnumerable<ComponenteCurricular> componentesTerritorioSaber = null;
 
-            if (ExisteComponentesTerritorioSaberTurma(componentesTS, request.TurmasId))
+            if (componentesTS.Any() && request.TurmasId.Any())
             {
-                componentesTerritorioSaber = request.TurmasId.Any() ? await componenteCurricularRepository.ListarComponentesTerritorioSaber(componentesTS, request.TurmasId) : null;
-
+                List<ComponenteCurricularTerritorioSaber> componentesTerritorioSaber =  (await componenteCurricularRepository.ObterComponentesTerritorioDosSaberes(request.TurmasId, componentesTS)).ToList();
                 foreach (var componenteTerritorio in componentesTerritorioSaber)
-                    componenteTerritorio.GrupoMatrizId = lstComponentes.Where(w=> w.Codigo == componenteTerritorio.Codigo).FirstOrDefault().GrupoMatrizId;
+                    componenteTerritorio.GrupoMatrizId = lstComponentes.Where(w=> w.Codigo == componenteTerritorio.CodigoComponenteCurricular).FirstOrDefault().GrupoMatrizId;
+                lstComponentes = ConcatenarComponenteTerritorio(lstComponentes, componentesTerritorioSaber);
             }
 
-            if (componentesTerritorioSaber != null)
-                lstComponentes = ConcatenarComponenteTerritorio(lstComponentes, componentesTerritorioSaber);
 
             if (lstComponentes != null && lstComponentes.Any())
             {
@@ -70,15 +67,17 @@ namespace SME.SR.Application
             return Enumerable.Empty<ComponenteCurricularPorTurma>();
         }
 
-        private bool ExisteComponentesTerritorioSaberTurma(string[] componentesTS, string[] turmasId)
+        public List<ComponenteCurricular> ConcatenarComponenteTerritorio(IEnumerable<ComponenteCurricular> componentes, IEnumerable<ComponenteCurricularTerritorioSaber> componentesTerritorio)
         {
-            return componentesTS != null && componentesTS.Length > 0 && turmasId != null && turmasId.Length > 0;
-        }
-
-        public List<ComponenteCurricular> ConcatenarComponenteTerritorio(IEnumerable<ComponenteCurricular> componentes, IEnumerable<ComponenteCurricular> componentesTerritorio)
-        {
-		var componentesConcatenados = componentes.Where(a => !componentesTerritorio.Any(x => x.Codigo == a.Codigo)).ToList();
-		return componentesConcatenados.Concat(componentesTerritorio).ToList();
+		var componentesConcatenados = componentes.Where(a => !componentesTerritorio.Any(x => x.CodigoComponenteCurricular == a.Codigo)).ToList();
+		return componentesConcatenados.Concat(componentesTerritorio.Select(ct => new ComponenteCurricular()
+                                                                            {
+                                                                                  Codigo = ct.CodigoComponenteCurricular,
+                                                                                  Descricao = ct.ObterDescricaoComponenteCurricular(),
+                                                                                  TerritorioSaber = true,
+                                                                                  GrupoMatrizId = ct.GrupoMatrizId,
+                                                                                  Frequencia = true
+                                                                            })).ToList();
         }
     }
 }
