5fb0e4b6d2df24d9667e58d8ef8b2802b54b832e
rafalosiamcom <rafael.losi@amcom.com.br>
2023-12-20 12:05:00 -0300

    logando etapas da ata final para identificar problema de lentidão

--- src/SME.SR.Application/UseCases/RelatorioConselhoClasseAtaFinalUseCase.cs
+++ src/SME.SR.Application/UseCases/RelatorioConselhoClasseAtaFinalUseCase.cs
@@ -10,19 +10,25 @@ namespace SME.SR.Application
     public class RelatorioConselhoClasseAtaFinalUseCase : IRelatorioConselhoClasseAtaFinalUseCase
     {
         private readonly IMediator mediator;
+        private int contador;
+        private Guid id;
 
         public RelatorioConselhoClasseAtaFinalUseCase(IMediator mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            contador = 0;
+            id = Guid.NewGuid();
         }
 
         public async Task Executar(FiltroRelatorioDto request)
         {
-
             request.RotaErro = RotasRabbitSGP.RotaRelatoriosComErroAtaFinalResultados;
             var filtros = request.ObterObjetoFiltro<FiltroConselhoClasseAtaFinalDto>();
             var mensagensErro = new StringBuilder();
+
+            await Logar("Inicio Processo");
             var relatoriosTurmas = await mediator.Send(new ObterRelatorioConselhoClasseAtaFinalPdfQuery(filtros));
+            await Logar("Dados Obtidos");
 
             if (relatoriosTurmas == null || !relatoriosTurmas.Any())
                 throw new NegocioException("Não há dados para o relatório de Ata Final de Resultados.");
@@ -30,14 +36,20 @@ namespace SME.SR.Application
             switch (filtros.TipoFormatoRelatorio)
             {
                 case TipoFormatoRelatorio.Xlsx:
+                    await Logar("Montando Excel");
                     var relatorioDto = await mediator.Send(new ObterRelatorioConselhoClasseAtaFinalExcelQuery() { ObjetoExportacao = relatoriosTurmas });
+                    await Logar("Excel Montado");
                     if (relatorioDto == null)
                         throw new NegocioException("Não foi possível transformar os dados obtidos em dados excel.");
 
+                    await Logar("Gerando Relatório Excel");
                     await mediator.Send(new GerarRelatorioAtaFinalExcelCommand(relatorioDto, relatoriosTurmas, "RelatorioAtasComColunaFinal", request.UsuarioLogadoRF));
+                    await Logar("Relatório Excel Gerado");
                     break;
                 case TipoFormatoRelatorio.Pdf:
+                    await Logar("Gerando Relatório PDF");
                     await mediator.Send(new GerarRelatorioAtaFinalHtmlParaPdfCommand("RelatorioAtasComColunaFinal", relatoriosTurmas, request.CodigoCorrelacao, mensagensErro.ToString()));
+                    await Logar("Relatório Excel PDF");
                     break;
                 case TipoFormatoRelatorio.Rtf:
                 case TipoFormatoRelatorio.Html:
@@ -52,5 +64,8 @@ namespace SME.SR.Application
                     throw new NegocioException($"Não foi possível exportar este relátorio para o formato {filtros.TipoFormatoRelatorio}");
             }
         }
+
+        private Task Logar(string mensagem)
+            => mediator.Send(new SalvarLogViaRabbitCommand($"{id}:{++contador}: {mensagem}", LogNivel.Informacao));
     }
 }
