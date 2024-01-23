5330cfb630588d1745be8b8d4410594ca211ebde
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-01-23 16:23:09 -0300

    Bug: 113151 Ajuste na geração do relatório de notas e conceitos aplicando algumas otimizações, como a busca de alunos nos dados do EOL e popular os dados de DREs, UEs, alunos, disciplinas para impressão no relatório.

--- src/SME.SR.Infra/Dtos/Relatorios/RelatorioNotasEConceitosFinais/RelatorioNotasEConceitosFinaisAnoDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/RelatorioNotasEConceitosFinais/RelatorioNotasEConceitosFinaisAnoDto.cs
@@ -4,11 +4,14 @@ namespace SME.SR.Infra
 {
     public class RelatorioNotasEConceitosFinaisAnoDto
     {
-        public RelatorioNotasEConceitosFinaisAnoDto(string nome)
+        public RelatorioNotasEConceitosFinaisAnoDto(string ano, string nome)
         {
+            Ano = ano;
             Nome = nome;
             Bimestres = new List<RelatorioNotasEConceitosFinaisBimestreDto>();
         }
+
+        public string Ano { get; set; }
         public string Nome { get; set; }
 
         public List<RelatorioNotasEConceitosFinaisBimestreDto> Bimestres { get; set; }
