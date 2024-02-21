2a132393b714a7489074a0f6565dcf5bd87bc487
DanylloPereira <danyllo.antunes@gmail.com>
2024-02-21 11:55:12 -0300

    112342 - Tratar DTOs geração relatório

--- src/SME.SR.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/Detalhado/FiltroRelatorioEncaminhamentoNAAPADetalhadoDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/Detalhado/FiltroRelatorioEncaminhamentoNAAPADetalhadoDto.cs
@@ -5,5 +5,6 @@
         public long[] EncaminhamentoNaapaIds { get; set; }
         public string UsuarioNome { get; set; }
         public string UsuarioRf { get; set; }
+        public ImprimirAnexosNAAPA ImprimirAnexos { get; set; }
     }
 }
