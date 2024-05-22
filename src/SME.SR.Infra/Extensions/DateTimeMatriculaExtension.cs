89103a741edbb9ba718819253a07d73d53b48b00
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-05-22 07:49:09 -0300

    Bug: 121884 Ajuste complementar na geração do relatório de frequência mensal considerando também o ano na verificação se o aluno está ativo no mês selecionado no filtro.

---
+++ src/SME.SR.Infra/Extensions/DateTimeMatriculaExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SME.SR.Infra.Extensions
+{
+    public static class DateTimeMatriculaExtension
+    {
+        public static bool AntecedeMesAno(this DateTime data, int mesReferencia, int anoReferencia)
+        {
+            if (data.Year < anoReferencia)
+                return true;
+
+            if (data.Year == anoReferencia && data.Month < mesReferencia)
+                return true;
+
+            if (data.Year == anoReferencia && data.Month == mesReferencia && data.Day < DateTime.DaysInMonth(anoReferencia, mesReferencia))
+                return true;
+
+            return false;
+        }
+
+        public static bool PosteriorOuEquivalenteMesAno(this DateTime data, int mesReferencia, int anoReferencia)
+        {
+            if (data.Year > anoReferencia)
+                return true;
+
+            if (data.Year == anoReferencia && data.Month >= mesReferencia)
+                return true;
+
+            mesReferencia = mesReferencia == 1 ? 12 : mesReferencia - 1;
+            if (mesReferencia == 12)
+                anoReferencia = -1;
+
+            if (data.Year == anoReferencia && data.Month == mesReferencia && data.Day == DateTime.DaysInMonth(anoReferencia, mesReferencia))
+                return true;
+
+            return false;
+        }
+    }
+}
