8daed13319e4bdc49a392daf1a829ced0dcb5c80
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-05-27 16:17:55 -0300

    Bug: 122379 Ajuste na geração do relatório analítico do sodagem, onde na consulta foram removidas tabelas desnecessárias, acertando os filtros. Tratamento de valor nulo ao tratar a quantidade de turmas no ano turma.

--- src/SME.SR.Data/Repositories/Sondagem/SondagemAnaliticaRepository.cs
+++ src/SME.SR.Data/Repositories/Sondagem/SondagemAnaliticaRepository.cs
@@ -966,7 +966,7 @@ namespace SME.SR.Data
 
                     foreach (var anoTurmaItem in perguntasPorAno)
                     {
-                        int quantidadeTurmas = totalTurmas?.FirstOrDefault(t => t.Ano == anoTurmaItem.Key.AnoTurma).Quantidade ?? 0;
+                        int quantidadeTurmas = totalTurmas?.FirstOrDefault(t => t.Ano == anoTurmaItem.Key.AnoTurma)?.Quantidade ?? 0;
                         var descricaoPergunta = ObterDescricaoPergunta(proficiencia, anoTurmaItem.Key.PerguntaDescricao, anoTurmaItem.Key.OrdemPergunta, int.Parse(anoTurmaItem.Key.AnoTurma));
                         if (string.IsNullOrEmpty(descricaoPergunta)) continue;
 
