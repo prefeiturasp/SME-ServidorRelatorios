2dafefef293de76382e2e532d51e27e3451e6f8e
DanylloPereira <danyllo.antunes@gmail.com>
2024-05-31 14:21:09 -0300

    122472 - Total do relatório impresso de matemática

--- src/SME.SR.Application/Queries/Sondagem/MatematicaAditivoMultiplicativoConsolidado/ObterSondagemMatematicaAditivoMultiplicativoConsolidado/ObterSondagemMatAditMultiConsolidadoQueryHandler.cs
+++ src/SME.SR.Application/Queries/Sondagem/MatematicaAditivoMultiplicativoConsolidado/ObterSondagemMatematicaAditivoMultiplicativoConsolidado/ObterSondagemMatAditMultiConsolidadoQueryHandler.cs
@@ -202,6 +202,8 @@ namespace SME.SR.Application
 
             GerarGrafico(relatorio, qtdAlunos, consideraNovaOpcaoRespostaSemPreenchimento);
 
+            relatorio.TotalDeAlunos = request.QuantidadeTotalAlunos;
+
             return relatorio;
         }
 
