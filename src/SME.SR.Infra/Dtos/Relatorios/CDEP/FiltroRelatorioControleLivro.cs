210d9be596536c692ffff99a7db1c9446cd3754c
cicero.santos <cicero.santos@spassu.com.br>
2025-08-08 17:07:12 -0300

    Bug do git

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/FiltroRelatorioControleLivro.cs
@@ -0,0 +1,17 @@
+﻿using SME.SR.Infra.CDEP;
+
+namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class FiltroRelatorioControleLivro
+    {
+        public string Solicitante { get; set; }
+        public string Tombo { get; set; }
+        public string Usuario { get; set; }
+        public string UsuarioRF { get; set; }
+        public SituacaoSolicitacaoItem SituacaoSolicitacaoItem { get; set; }
+        public SituacaoEmprestimo SituacaoEmprestimo { get; set; }
+        public ModeloRelatorio Modelo { get; set; }
+        public bool SomenteDevolvidos { get; set; }
+        public long[] TiposAcervosPermitidos { get; set; }
+    }
+}
