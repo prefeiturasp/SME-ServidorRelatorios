51e8cc11a60a9710758b03ca756ae3e4baa96f95
cicero.santos <cicero.santos@spassu.com.br>
2025-08-08 17:07:12 -0300

    Bug do git

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/AcervoSolicitacaoSinteticoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class AcervoSolicitacaoSinteticoDto
+    {
+        public string Tombo { get; set; }
+        public string Titulo { get; set; }
+        public int? QuantidadeEmprestimos { get; set; }
+    }
+}
