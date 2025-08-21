339c40b5c9d850a6a134d0f6058dcb2bcf3c50d8
cicero.santos <cicero.santos@spassu.com.br>
2025-08-21 14:11:32 -0300

    Dto faltando

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/AcervoDevolucaoDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class AcervoDevolucaoDto
+    {
+        public string Solicitante { get; set; }
+        public string Tombo { get; set; }
+        public string Titulo { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public int DiasEmprestimo
+        {
+            get
+            {
+                TimeSpan duracao = DataDevolucao - DataEmprestimo;
+                return duracao.Days;
+            }
+        }
+    }
+}
