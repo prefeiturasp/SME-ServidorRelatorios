dc26a2bf02ce18c0bbb85a18f78669d51b240b81
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-08-27 09:46:38 -0300

    Adicionar DTOs faltando

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/FiltroRelatorioControleEditora.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class FiltroRelatorioControleEditora
+    {
+        public string Usuario { get; set; }
+        public string UsuarioRF { get; set; }
+        public List<int> EditoraId { get; set; }
+    }
+}
