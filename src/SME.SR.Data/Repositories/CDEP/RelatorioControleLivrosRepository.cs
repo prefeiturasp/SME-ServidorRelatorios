4424beea7a81256feb18643489e7c1662bb4f7f0
Diego Moreno <dierudito@gmail.com>
2025-08-27 11:05:28 -0300

    Refatora parâmetro solicitante para int na devolucao livro do cdep
    O parâmetro "solicitante" foi alterado de List<int> para int? no repositório, na interface e no DTO para o método "ObterRelatorioControleDevolucaoLivros". Isso simplifica o filtro para suportar um único solicitante opcional em vez de uma lista.

--- src/SME.SR.Data/Repositories/CDEP/RelatorioControleLivrosRepository.cs
+++ src/SME.SR.Data/Repositories/CDEP/RelatorioControleLivrosRepository.cs
@@ -213,7 +213,7 @@ namespace SME.SR.Data
             return await conexao.QueryAsync<ControleAcervoAutorDTO>(query.ToString(), parametros);
         }
 
-        public async Task<IEnumerable<AcervoDevolucaoDto>> ObterRelatorioControleDevolucaoLivros(long[] tiposAcervosPermitidos, List<int> solicitante, bool? somenteAtrasados = false)
+        public async Task<IEnumerable<AcervoDevolucaoDto>> ObterRelatorioControleDevolucaoLivros(long[] tiposAcervosPermitidos, int? solicitante, bool? somenteAtrasados = false)
         {
             var query = new StringBuilder();
             query.AppendLine(@"
@@ -251,9 +251,9 @@ namespace SME.SR.Data
                 }
             }
 
-            if (solicitante?.Count > 0)
+            if (solicitante != null && solicitante > 0)
             {
-                query.AppendLine(" AND u.id = ANY(@Solicitante)");
+                query.AppendLine(" AND u.id = @Solicitante");
                 parametros.Add("Solicitante", solicitante);
             }
 
