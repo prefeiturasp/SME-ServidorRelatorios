ff964b3427b61b531fd00637a357d379497859ed
Diego Moreno <dierudito@gmail.com>
2025-11-06 13:34:46 -0300

    Arquivo faltantes

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/FiltroRelatorioTitulosMaisPesquisados.cs
@@ -0,0 +1,15 @@
+﻿using SME.SR.Infra.CDEP;
+using System;
+using System.Collections.Generic;
+
+namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class FiltroRelatorioTitulosMaisPesquisados
+    {
+        public string Usuario { get; set; }
+        public string UsuarioRF { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public List<TipoAcervo> TipoAcervos { get; set; }
+    }
+}
---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/RelatorioTitulosMaisPesquisadosDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class RelatorioTitulosMaisPesquisadosDto
+    {
+        public string TermoNormalizado { get; set; }
+        public long Quantidade { get; set; }
+    }
+}
