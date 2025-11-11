5e76f16c721e2f02e5d8d3666d4d23fa3e8d7ceb
Diego Moreno <dierudito@gmail.com>
2025-11-11 11:13:05 -0300

    Inclui arquivos faltantes

---
+++ src/SME.SR.Infra/Dtos/Relatorios/CDEP/FiltroRelatorioControleDownloadAcervo.cs
@@ -0,0 +1,12 @@
+﻿using SME.SR.Infra.CDEP;
+
+namespace SME.SR.Infra.Dtos.Relatorios.CDEP
+{
+    public class FiltroRelatorioControleDownloadAcervo
+    {
+        public string Usuario { get; set; }
+        public string UsuarioRF { get; set; }
+        public string Titulo { get; set; }
+        public TipoAcervo TipoAcervo { get; set; }
+    }
+}
