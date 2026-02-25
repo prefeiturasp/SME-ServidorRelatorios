d0de35e6f76d7260299c42adba0fd22723647926
wesleygyn <wesley_gyn@yahoo.com.br>
2026-02-25 17:39:55 -0300

    AB#143304 Criar exportação para PDF Gráfico

---
+++ src/SME.SR.Infra/Dtos/NovoSondagem/GraficoSondagemDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SR.Infra.Dtos.NovoSondagem
+{
+    public class GraficoSondagemDto
+    {
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public List<GraficoBarraDto> Barras { get; set; } = new List<GraficoBarraDto>();
+    }
+
+    public class GraficoBarraDto
+    {
+        public string Legenda { get; set; }
+        public string CorFundo { get; set; }
+        public string CorTexto { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
