4b93ec38859156d9287dcb490cfd50ffe646fd70
Diego Moreno <dierudito@gmail.com>
2026-03-19 11:42:13 -0300

    feat: Permite DataFim nula e ajusta expansão de datas
    Altera DataFim para DateTime? em DataAulaTurmaRelatorioCodafDto, permitindo valores nulos. Atualiza lógica de expansão de datas em GerarPlanilhaCodafCommandHandler para tratar períodos sem DataFim, garantindo inclusão correta das datas de início.
    
    AB#145480

--- src/SME.SR.Infra/Dtos/Relatorios/Conecta/DataAulaTurmaRelatorioCodafDto.cs
+++ src/SME.SR.Infra/Dtos/Relatorios/Conecta/DataAulaTurmaRelatorioCodafDto.cs
@@ -5,6 +5,6 @@ namespace SME.SR.Infra.Dtos.Relatorios.Conecta
     public class DataAulaTurmaRelatorioCodafDto
     {
         public DateTime DataInicio { get; set; }
-        public DateTime DataFim { get; set; }
+        public DateTime? DataFim { get; set; }
     }
 }
